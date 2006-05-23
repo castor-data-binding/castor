2182bdd380dadd8e46a8950f9a278335e671af1d
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2006-05-23 12:42:31 +0000

    Refactored JUnit test case for XML bugs, adding a test method for marshalling.

--- castor/trunk/src/bugs/xml/template/TestTemplate.java
+++ castor/trunk/src/bugs/xml/template/TestTemplate.java
@@ -1,13 +1,18 @@
 package xml.template;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 import junit.framework.TestCase;
 
 import org.exolab.castor.mapping.Mapping;
+import org.exolab.castor.xml.Marshaller;
 import org.exolab.castor.xml.Unmarshaller;
 import org.xml.sax.InputSource;
 
 public final class TestTemplate extends TestCase {
     
+    private static final String OUTPUT_FILE = "out.xml";
     private static final String SAMPLE_FILE = "entity.xml";
     private static final String MAPPING_FILE = "mapping.xml";
     
@@ -23,7 +28,7 @@ public final class TestTemplate extends TestCase {
      * Test method.
      * @throws Exception For any exception thrown.
      */
-    public void testQueryEntityOne() throws Exception {
+    public void testUnmarshalEntity() throws Exception {
         Mapping mapping = new Mapping();
         mapping.loadMapping(getClass().getResource(MAPPING_FILE).toExternalForm());
         
@@ -35,4 +40,23 @@ public final class TestTemplate extends TestCase {
         assertEquals (1, entity.getId().intValue());
         assertEquals("name", entity.getName());
     }
+    
+    /**
+     * Test method.
+     * @throws Exception For any exception thrown.
+     */
+    public void testMarshalEntity() throws Exception {
+        Mapping mapping = new Mapping();
+        mapping.loadMapping(getClass().getResource(MAPPING_FILE).toExternalForm());
+        
+        PrintWriter out = new PrintWriter(new FileWriter(OUTPUT_FILE));
+        Marshaller marshaller = new Marshaller (out);
+        marshaller.setMapping(mapping);
+        
+        Entity entity = new Entity();
+        entity.setId(new Integer(100));
+        entity.setName("entity 100");
+        marshaller.marshal(entity);
+    }
+    
 }
