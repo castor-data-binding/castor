2ac2fb22a42509eb0a21c9568df1e476e5389def
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2006-06-08 19:29:15 +0000

    Release preparations (1.0.1)

--- castor/trunk/src/main/java/org/exolab/castor/util/Version.java
+++ castor/trunk/src/main/java/org/exolab/castor/util/Version.java
@@ -67,12 +67,12 @@ public final class Version {
     /**
      * The version number
      */
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
         
     /**
      * The version date.
      */
-    public static final String VERSION_DATE  = "20060406";
+    public static final String VERSION_DATE  = "20060608";
     
     /**
      * The version number with build information
