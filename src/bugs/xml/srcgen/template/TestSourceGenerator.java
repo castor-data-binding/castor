6b2ca2b88a0fe16b25fc426e9a2e2579d823ec60
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2006-09-13 12:17:30 +0000

    Added new template for XM code generator.

---
+++ src/bugs/xml/srcgen/template/TestSourceGenerator.java
@@ -0,0 +1,28 @@
+package xml.srcgen.template;
+
+// import java.util.Properties;
+
+import junit.framework.TestCase;
+
+import org.exolab.castor.builder.SourceGenerator;
+import org.xml.sax.InputSource;
+
+public class TestSourceGenerator extends TestCase
+{
+
+    public void testGeneration() throws Exception {
+        SourceGenerator generator = new SourceGenerator();
+        String xmlSchema = getClass().getResource("test.xsd").toExternalForm();
+        InputSource inputSource = new InputSource(xmlSchema);
+//        generator.setBinding(new InputSource(getClass().getResource("binding.xml").toExternalForm()));
+        generator.setDestDir("./src/bugs");
+        generator.setSuppressNonFatalWarnings(true);
+        
+//        Properties properties = new Properties();
+//        properties.load(getClass().getResource("special-castorbuilder.properties").openStream());
+//        generator.setDefaultProperties(properties);
+        
+        generator.generateSource(inputSource, "xml.srcgen.template.generated");
+    }
+
+}
