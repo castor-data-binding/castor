43ade5f4aeb43fdc2ffa9b2706b55f3f01ce5d10
ekuns <ekuns@b24b0d9a-6811-0410-802a-946fa971d308>
2006-10-07 00:57:10 +0000

    Apply change for CASTOR-1417 so hashCode() source will be generated with srcgen. Now, when equals() is generated, hashCode() will also be generated.

--- castor/trunk/src/main/java/org/exolab/castor/builder/SourceFactory.java
+++ castor/trunk/src/main/java/org/exolab/castor/builder/SourceFactory.java
@@ -607,7 +607,7 @@ public class SourceFactory extends BaseFactory {
         //create equals() method?
         if (component.hasEquals()) {
             createEqualsMethod(jClass);
-//            createHashCodeMethod(jClass);
+            createHashCodeMethod(jClass);
         }
         //implements CastorTestable?
         if (_testable)
@@ -1031,62 +1031,67 @@ public class SourceFactory extends BaseFactory {
 
     } //-- createUnmarshalMethods
 
-//    /**
-//     * Create an "hashCode" method on the given JClass.
-//     *
-//     * @param jclass the JClass in wich we create the hashCode method.
-//     */
-//    public static void createHashCodeMethod(JClass jclass) {
-//        if (jclass == null) {
-//            throw new IllegalArgumentException("JClass must not be null");
-//        }
-//
-//        // The argument is not null
-//        JField[] fields = jclass.getFields();
-//
-//        // Creates the method signature
-//        JMethod jMethod = new JMethod(JType.Int, "hashCode");
-//        jMethod.setComment("Override the java.lang.Object.hashCode method");
-//
-//        // The hashCode method has no arguments
-//        jclass.addMethod(jMethod);
-//
-//        JSourceCode jsc = jMethod.getSourceCode();
-//
-//        // The following steps came from Effective Java Programming Language Guide by Joshua Bloch, chapter 3
-//        jsc.add("int result = 17;");
-//
-//        for (int i = 0; i <fields.length; i++) {
-//            JField temp = fields[i];
-//            // If the field is an object the hashCode method is called recursively
-//
-//            JType type = temp.getType();
-//            String name = temp.getName();
-//            if (type.isPrimitive()) {
-//                if (type == JType.Boolean) {
-//                    jsc.add("result = 37 * result + (" + name + "?0:1);");
-//                } else
-//                if ((type == JType.Byte) ||
-//                    (type == JType.Int) ||
-//                    (type == JType.Short))
-//                {
-//                    jsc.add("result = 37 * result + " + name + ";");
-//                } else if (type == JType.Long) {
-//                    jsc.add("result = 37 * result + (int)(" + name + "^(" + name + ">>>32));");
-//                } else if (type == JType.Float) {
-//                    jsc.add("result = 37 * result + Float.floatToIntBits(" + name + ");");
-//                } else if (type == JType.Double) {
-//                    jsc.add("long tmp = Double.doubleToLongBits(" + name + ");");
-//                    jsc.add("result = 37 * result + (int)(tmp^(tmp>>>32));");
-//                }
-//            }   else {
-//                // Calculates hashCode in a recursive manner.
-//                jsc.add("result = 37 * result + " + name + ".hashCode();");
-//            }
-//        }
-//        jsc.add("");
-//        jsc.add("return result;");
-//    }   //createHashCodeMethod
+    /**
+     * Create an "hashCode" method on the given JClass.
+     *
+     * @param jclass the JClass in wich we create the hashCode method.
+     */
+    public static void createHashCodeMethod(JClass jclass) {
+        if (jclass == null) {
+            throw new IllegalArgumentException("JClass must not be null");
+        }
+
+        // The argument is not null
+        JField[] fields = jclass.getFields();
+
+        // Creates the method signature
+        JMethod jMethod = new JMethod("hashCode", JType.INT, "a hash code value for the object.");
+        jMethod.setComment("Overrides the java.lang.Object.hashCode method.\n" +
+                           "<p>\n" +
+                           "The following steps came from <b>Effective Java Programming Language Guide</b> " +
+                           "by Joshua Bloch, Chapter 3");
+
+        // The hashCode method has no arguments
+        jclass.addMethod(jMethod);
+
+        JSourceCode jsc = jMethod.getSourceCode();
+
+        jsc.add("int result = 17;");
+        jsc.add("");
+        jsc.add("long tmp;");
+
+        for (int i = 0; i <fields.length; i++) {
+            JField temp = fields[i];
+            // If the field is an object the hashCode method is called recursively
+
+            JType type = temp.getType();
+            String name = temp.getName();
+            if (type.isPrimitive()) {
+                if (type == JType.BOOLEAN) {
+                    // Skip the _has_* variables only if they represent a primitive that may or may not be present
+                    if (!name.startsWith("_has_") || jclass.getField(name.substring(5)) != null) {
+                        jsc.add("result = 37 * result + (" + name + "?0:1);");
+                    }
+                } else if (type == JType.BYTE || type == JType.INT || type == JType.SHORT) {
+                    jsc.add("result = 37 * result + " + name + ";");
+                } else if (type == JType.LONG) {
+                    jsc.add("result = 37 * result + (int)(" + name + "^(" + name + ">>>32));");
+                } else if (type == JType.FLOAT) {
+                    jsc.add("result = 37 * result + Float.floatToIntBits(" + name + ");");
+                } else if (type == JType.DOUBLE) {
+                    jsc.add("tmp = Double.doubleToLongBits(" + name + ");");
+                    jsc.add("result = 37 * result + (int)(tmp^(tmp>>>32));");
+                }
+            } else {
+                // Calculates hashCode in a recursive manner.
+                jsc.add("if (" + name + " != null) {");
+                jsc.add("   result = 37 * result + " + name + ".hashCode();");
+                jsc.add("}");
+            }
+        }
+        jsc.add("");
+        jsc.add("return result;");
+    }   //createHashCodeMethod
 
     /**
      * Create an 'equals' method on the given
@@ -1098,9 +1103,8 @@ public class SourceFactory extends BaseFactory {
             throw new IllegalArgumentException("JClass must not be null");
 
         JField[] fields = jclass.getFields();
-        JMethod jMethod = new JMethod("equals", JType.BOOLEAN, "true if the objects are equal");
-        jMethod.setComment("Override the java.lang.Object.equals method");
-        jMethod.setComment("Note: hashCode() has not been overriden");
+        JMethod jMethod = new JMethod("equals", JType.BOOLEAN, "true if the objects are equal.");
+        jMethod.setComment("Overrides the java.lang.Object.equals method.");
         jMethod.addParameter(new JParameter(SGTypes.Object, "obj"));
 
         if (_config.useJava50()) {
