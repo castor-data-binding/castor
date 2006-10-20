6254a126db96db3ff9db5a9f57684d17d0a6bd4c
ekuns <ekuns@b24b0d9a-6811-0410-802a-946fa971d308>
2006-10-20 03:45:38 +0000

    Enhance UNIX CTFRun.sh script (CASTOR-1641)

--- bin/CTFRun.sh
+++ bin/CTFRun.sh
@@ -25,8 +25,51 @@ CLASSPATH=`echo $LIB_D/*.jar | tr ' ' ':'`:$CLASSPATH
 # Remove the output of the previous test
 rm -rf $BUILD_D/tests/output/
 
+# No argument at all runs the master test suite
+case "$1" in
+    help)
+        echo "Usage:"
+        echo "   Run the master test suite with default arguments:"
+        echo "       $0"
+        echo "   Run a specific test or subtree of tests:"
+        echo "       $0 [ normal CTF arguments ] ../src/tests/xml/path/to/test/tree"
+        echo "   Run a test suite with optional extra arguments:"
+        echo "       $0 master|regression|bugs|all  [ normal CTF arguments ]"
+        echo ""
+        echo "The normal CTF arguments are:"
+        echo "   -verbose      - Display verbose status-of-test information"
+        echo "   -printStack   - Print the stack dump of all exceptions that occur"
+        echo "   -text         - Do not use the JUnit GUI"
+        echo "   -seed <seed>  - Use the specified initial random seed"
+        echo "   -help         - Print usage from the CTF suite program"
+        echo
+
+        exit 0
+        ;;
+    '')
+        shift
+        WHICHTESTS=../src/tests/xml/MasterTestSuite
+        ;;
+    master)
+        shift
+        WHICHTESTS=../src/tests/xml/MasterTestSuite
+        ;;
+    regression)
+        shift
+        WHICHTESTS=../src/tests/xml/RegressionTestSuite
+        ;;
+    bugs)
+        shift
+        WHICHTESTS=../src/tests/xml/KnownBugs
+        ;;
+    all)
+        shift
+        WHICHTESTS=../src/tests/xml/
+        ;;
+    *)
+        WHICHTESTS=
+        ;;
+esac
+
 # run the test
-$JAVA -cp $CLASSPATH org.exolab.castor.tests.framework.CastorTestSuiteRunner $*
-
-
-
+$JAVA -cp $CLASSPATH org.exolab.castor.tests.framework.CastorTestSuiteRunner $WHICHTESTS $*
