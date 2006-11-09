a098f328500de3021f1169a1c482bacb3d3423bf
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2006-11-09 16:34:26 +0000

    Added a CTF test to check that IDREF object structures are correctly unmarshalled.

---
+++ src/tests/xml/MasterTestSuite/mapping/identity-references/Node.java
@@ -0,0 +1,54 @@
+import java.util.Iterator;
+import java.util.List;
+
+public class Node {
+    protected int nodeNum;
+
+    protected List siblings;
+
+    public Node() {
+    };
+
+    public Node(int nodeNum, List siblings) {
+        this.nodeNum = nodeNum;
+        this.siblings = siblings;
+    }
+
+    public int getNodeNum() {
+        return nodeNum;
+    }
+
+    public void setNodeNum(int nodeNum) {
+        this.nodeNum = nodeNum;
+    }
+
+    public List getSiblings() {
+        return siblings;
+    }
+
+    public void setSiblings(List siblings) {
+        this.siblings = siblings;
+    }
+
+    public boolean equals(Object obj) {
+        Node node = (Node) obj;
+        if (node.getNodeNum() != getNodeNum()) {
+            return false;
+        }
+        
+        Iterator siblingIter = getSiblings().iterator();
+        Iterator iter = node.getSiblings().iterator();
+        while (iter.hasNext()) {
+            Node siblingNode = (Node) iter.next();
+            Node sibling = (Node) siblingIter.next();
+            
+            if (siblingNode.getNodeNum() != sibling.getNodeNum()) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    
+}
