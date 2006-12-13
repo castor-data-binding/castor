4747fa8c34dbc70f883c6fd3e9b1f0676fbe32c0
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2006-12-13 20:09:14 +0000

    Moved Ant JARs into bin/lib (and thus out of lib), and amended build scripts accordingly (CASTOR-1693).

--- bin/build11.bat
+++ bin/build11.bat
@@ -1,6 +1,11 @@
 @echo off
 REM $Id$
 set JAVA=%JAVA_HOME%\bin\java
-set CP=..\lib\ant-1.6.jar;..\lib\ant-1.6-launcher.jar;build\classes;%JAVA_HOME%\lib\classes.zip;..\lib\xerces-J_1.4.0.jar
+REM Ant infrastructure
+set "CP=lib\ant-1.6.jar;lib\ant-1.6-launcher.jar;build\classes"
+REM Sun's JDK tools
+set "CP=%CP;%JAVA_HOME%\lib\classes.zip"
+REM Xerces 1.4, disable for JDK 1.4 and above
+set "CP=%CP%;..\lib\xerces-J_1.4.0.jar"
 %JAVA% -nojit -mx128M -classpath %CP% -Dant.home=lib org.apache.tools.ant.Main -Dexcludes="**/package.html,org/exolab/castor/mapping/loader/J2CollectionHandlers.java,org/exolab/castor/util/IteratorEnumeration.java,org/exolab/castor/xml/dtd/**,org/exolab/castor/jdo/**,org/exolab/castor/gui/**,org/exolab/castor/tools/**,org/exolab/castor/dax/**,org/exolab/castor/persist/**,org/exolab/castor/dtx/**" -Dexampleexcludes="SourceGenerator/*,dax/*,**/jdo/**,**/dtx/**,**/oes/**,**/servlet/**,**/myapp/**" -Dtestexcludes="**/package.html,**/jdo/**,**/introspection/**,**/tests/framework/**" -Dapipackagenames="org.exolab.castor.xml,org.exolab.castor.dsml,org.exolab.castor.mapping" -Dallpackagenames="org.exolab.castor.xml.*,org.exolab.castor.dsml.*,org.exolab.castor.tools.*,org.exolab.castor.types.*,org.exolab.castor.util.*,org.exolab.castor.builder.*,org.exolab.castor.mapping.*" %1 %2 %3 %4 %5 %6 -buildfile ..\src\build.xml
 rem ,**/xml/xml2java/**,
