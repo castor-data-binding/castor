37ef0f4795a11c4e5d28eb3cb826a4b73ae91003
ekuns <ekuns@b24b0d9a-6811-0410-802a-946fa971d308>
2006-12-14 03:07:37 +0000

    unable to create a file called Con.java on Windows (CASTOR-644)

--- src/main/java/org/exolab/castor/builder/SingleClassGenerator.java
+++ src/main/java/org/exolab/castor/builder/SingleClassGenerator.java
@@ -400,6 +400,17 @@ public class SingleClassGenerator {
                     + " schema element.";
             sInfo.getDialog().notify(warn);
         }
+
+        if (JNaming.isReservedByWindows(nameToCompare)) {
+            // FIXME:  We should fail under Windows and warn under other OSes
+            String warn = "'" + nameToCompare + "' is reserved by the Windows filesystem and"
+                    + " cannot be\nused as a class name.  Windows will not allow you to create"
+                    + " a file with this\nname.  You will have to use a mapping file or change"
+                    + " the name of the conflicting\nschema element.  For more information,"
+                    + " see\nhttp://msdn.microsoft.com/library/default.asp?"
+                    + "url=/library/en-us/fileio/fs/naming_a_file.asp";
+            sInfo.getDialog().notify(warn);
+        }
     }
 
     /**
