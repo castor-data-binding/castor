af617f362ff7c4a427ba3b1c5e0f6bf0f900c347
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2006-12-23 21:05:21 +0000

    Preparations for 1.1 M1.

--- src/main/java/org/exolab/castor/util/Version.java
+++ src/main/java/org/exolab/castor/util/Version.java
@@ -67,12 +67,12 @@ public final class Version {
     /**
      * The version number
      */
-    public static final String VERSION = "1.0.5";
+    public static final String VERSION = "1.1-M1";
         
     /**
      * The version date.
      */
-    public static final String VERSION_DATE  = "20061111";
+    public static final String VERSION_DATE  = "20061223";
     
     /**
      * The version number with build information
