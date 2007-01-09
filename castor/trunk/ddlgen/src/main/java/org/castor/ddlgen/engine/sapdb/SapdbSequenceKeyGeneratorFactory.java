83ed41e20e118192f409983b1dcced405803ff12
ekuns <ekuns@b24b0d9a-6811-0410-802a-946fa971d308>
2007-01-09 04:43:08 +0000

    Clean up Java 5 build complaints in ddlgen (CASTOR-1817)

--- castor/trunk/ddlgen/src/main/java/org/castor/ddlgen/engine/sapdb/SapdbSequenceKeyGeneratorFactory.java
+++ castor/trunk/ddlgen/src/main/java/org/castor/ddlgen/engine/sapdb/SapdbSequenceKeyGeneratorFactory.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2006 Le Duc Bao, Ralf Joachim
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -24,7 +24,7 @@ import org.castor.ddlgen.schemaobject.KeyGenerator;
 
 /**
  * Factory class for SEQUENCE key generators for SapDB.
- * 
+ *
  * @author <a href="mailto:leducbao AT gmail DOT com">Le Duc Bao</a>
  * @author <a href="mailto:ralf DOT joachim AT syscon-world DOT de">Ralf Joachim</a>
  * @version $Revision: 5951 $ $Date: 2006-04-25 16:09:10 -0600 (Tue, 25 Apr 2006) $
@@ -40,9 +40,9 @@ public final class SapdbSequenceKeyGeneratorFactory extends SequenceKeyGenerator
         StringBuffer buff = new StringBuffer();
         String tableName = sequenceKey.getTable().getName();
         String pkList = toPrimaryKeyList(key.getTable());
-        String sequenceName = MessageFormat.format(sequenceKey.getSequence(), 
-                new String[]{tableName, pkList});
-        
+        String sequenceName = MessageFormat.format(sequenceKey.getSequence(),
+                new Object[]{tableName, pkList});
+
         String newline = conf.getStringValue(
                 DDLGenConfiguration.NEWLINE_KEY, DDLGenConfiguration.DEFAULT_NEWLINE);
         String indent = conf.getStringValue(
@@ -67,20 +67,20 @@ public final class SapdbSequenceKeyGeneratorFactory extends SequenceKeyGenerator
 
             String triggerTemp = conf.getStringValue(
                     DDLGenConfiguration.TRIGGER_TEMPLATE_KEY, "");
-    
+
             triggerTemp = triggerTemp.replaceAll("<trigger_name>", triggerName);
             triggerTemp = triggerTemp.replaceAll("<sequence_name>", sequenceName);
-            triggerTemp = triggerTemp.replaceAll("<table_name>", tableName);            
-            triggerTemp = triggerTemp.replaceAll("<pk_name>", pkList);            
-            triggerTemp = triggerTemp.replaceAll("<pk_type>", pkTypeList);            
+            triggerTemp = triggerTemp.replaceAll("<table_name>", tableName);
+            triggerTemp = triggerTemp.replaceAll("<pk_name>", pkList);
+            triggerTemp = triggerTemp.replaceAll("<pk_type>", pkTypeList);
             buff.append(newline);
             buff.append(newline);
             buff.append(triggerTemp);
         }
-        
+
         return buff.toString();
     }
-    
+
     /**
      * {@inheritDoc}
      */
