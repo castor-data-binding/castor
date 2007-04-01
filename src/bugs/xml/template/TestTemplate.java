d745c88c4d9fd37210695e8bfd1b70650b2bb109
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2007-04-01 16:01:36 +0000

    Simplifications for (un)marshalling test case(s).

--- src/bugs/xml/template/TestTemplate.java
+++ src/bugs/xml/template/TestTemplate.java
@@ -1,20 +1,16 @@
 package xml.template;
 
-import java.io.FileWriter;
-import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import junit.framework.TestCase;
 
-import org.exolab.castor.mapping.Mapping;
 import org.exolab.castor.xml.Marshaller;
 import org.exolab.castor.xml.Unmarshaller;
 import org.xml.sax.InputSource;
 
 public final class TestTemplate extends TestCase {
     
-    private static final String OUTPUT_FILE = "out.xml";
-    private static final String SAMPLE_FILE = "entity.xml";
-    private static final String MAPPING_FILE = "mapping.xml";
+    private static final String SAMPLE_FILE = "input.xml";
     
     public TestTemplate() {
         super();
@@ -29,11 +25,7 @@ public final class TestTemplate extends TestCase {
      * @throws Exception For any exception thrown.
      */
     public void testUnmarshalEntity() throws Exception {
-        Mapping mapping = new Mapping();
-        mapping.loadMapping(getClass().getResource(MAPPING_FILE).toExternalForm());
-        
         Unmarshaller unmarshaller = new Unmarshaller (Entity.class);
-        unmarshaller.setMapping(mapping);
         
         Entity entity = (Entity) unmarshaller.unmarshal(new InputSource(getClass().getResource(SAMPLE_FILE).toExternalForm()));
         assertNotNull (entity);
@@ -46,17 +38,16 @@ public final class TestTemplate extends TestCase {
      * @throws Exception For any exception thrown.
      */
     public void testMarshalEntity() throws Exception {
-        Mapping mapping = new Mapping();
-        mapping.loadMapping(getClass().getResource(MAPPING_FILE).toExternalForm());
         
-        PrintWriter out = new PrintWriter(new FileWriter(OUTPUT_FILE));
+        StringWriter out = new StringWriter();
         Marshaller marshaller = new Marshaller (out);
-        marshaller.setMapping(mapping);
         
         Entity entity = new Entity();
         entity.setId(new Integer(100));
         entity.setName("entity 100");
         marshaller.marshal(entity);
+        
+        System.out.println(out.toString());
     }
     
 }
