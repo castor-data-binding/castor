74aa12ddd7615d5a3c65f860fcb172ae3eac4580
rjoachim <rjoachim@b24b0d9a-6811-0410-802a-946fa971d308>
2007-04-23 22:23:38 +0000

    Move examples into a separate module: Fix package changes (CASTOR-1952)

--- examples/src/main/java/tools/CastorMapper.java
+++ examples/src/main/java/tools/CastorMapper.java
@@ -4,7 +4,7 @@
  * To change the template for this generated file go to
  * Window - Preferences - Java - Code Generation - Code and Comments
  */
-package MappingTool;
+package tools;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
