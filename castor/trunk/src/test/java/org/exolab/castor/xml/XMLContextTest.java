f66e44e80ed96135fb5524bd876ba71de966c7a4
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2007-05-02 09:58:55 +0000

    Commented out artefacts from a commit set that should not have been committed (yet).

--- castor/trunk/src/test/java/org/exolab/castor/xml/XMLContextTest.java
+++ castor/trunk/src/test/java/org/exolab/castor/xml/XMLContextTest.java
@@ -1,7 +1,7 @@
 package org.exolab.castor.xml;
 
 import org.castor.test.entity.Entity;
-import org.castor.test.entity.EntitySecond;
+//import org.castor.test.entity.EntitySecond;
 import org.exolab.castor.mapping.Mapping;
 import org.xml.sax.InputSource;
 
@@ -16,7 +16,7 @@ public class XMLContextTest extends TestCase {
      * @throws Exception
      */
     public void testXMLContextByPackage() throws Exception {
-        XMLContext context = XMLContext.newInstance();
+        XMLContext context = new XMLContext();
         context.addPackage("org.castor.test.entity");
         assertNotNull (context);
         
@@ -41,7 +41,7 @@ public class XMLContextTest extends TestCase {
         Mapping mapping = XMLContext.createMapping();
         mapping.loadMapping(new InputSource(getResource(MAPPING_FILE)));
         
-        XMLContext context = XMLContext.newInstance();
+        XMLContext context = new XMLContext();
         context.addMapping(mapping);
         assertNotNull (context);
         
@@ -56,32 +56,32 @@ public class XMLContextTest extends TestCase {
         
     }
 
-    public void testGetDescriptor() throws Exception {
-        Mapping mapping = XMLContext.createMapping();
-        mapping.loadMapping(new InputSource(getResource(MAPPING_FILE)));
-        
-        XMLContext context = XMLContext.newInstance();
-        context.addMapping(mapping);
-        assertNotNull (context);
-        
-        XMLClassDescriptor descriptor = context.getDescriptor(Entity.class);
-        assertNotNull(descriptor);
-        assertEquals(Entity.class, descriptor.getJavaClass());
-        
-    }
-
-    public void testGetMissingDescriptor() throws Exception {
-        Mapping mapping = XMLContext.createMapping();
-        mapping.loadMapping(new InputSource(getResource(MAPPING_FILE)));
-        
-        XMLContext context = XMLContext.newInstance();
-        context.addMapping(mapping);
-        assertNotNull (context);
-        
-        XMLClassDescriptor descriptor = context.getDescriptor(EntitySecond.class);
-        assertNull(descriptor);
-    }
-
+//    public void testGetDescriptor() throws Exception {
+//        Mapping mapping = XMLContext.createMapping();
+//        mapping.loadMapping(new InputSource(getResource(MAPPING_FILE)));
+//        
+//        XMLContext context = XMLContext.newInstance();
+//        context.addMapping(mapping);
+//        assertNotNull (context);
+//        
+//        XMLClassDescriptor descriptor = context.getDescriptor(Entity.class);
+//        assertNotNull(descriptor);
+//        assertEquals(Entity.class, descriptor.getJavaClass());
+//        
+//    }
+//
+//    public void testGetMissingDescriptor() throws Exception {
+//        Mapping mapping = XMLContext.createMapping();
+//        mapping.loadMapping(new InputSource(getResource(MAPPING_FILE)));
+//        
+//        XMLContext context = XMLContext.newInstance();
+//        context.addMapping(mapping);
+//        assertNotNull (context);
+//        
+//        XMLClassDescriptor descriptor = context.getDescriptor(EntitySecond.class);
+//        assertNull(descriptor);
+//    }
+//
     /**
      * Returns absolute path for resource.
      * @param resource Relative path to resource
