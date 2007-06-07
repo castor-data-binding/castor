8dba4486edf541e8f8fb4cd469836f71c42f3635
rjoachim <rjoachim@b24b0d9a-6811-0410-802a-946fa971d308>
2007-06-07 22:28:46 +0000

    SVN moved JDO files from src/test/java into cpa/src/test/java (CASTOR-2006)

--- cpa/src/test/java/org/exolab/castor/jdo/oql/TestLexer.java
+++ cpa/src/test/java/org/exolab/castor/jdo/oql/TestLexer.java
@@ -52,7 +52,7 @@ import junit.framework.TestCase;
  * @author  <a href="nissim@nksystems.com">Nissim Karpenstein</a>
  * @version $Revision$ $Date: 2006-04-29 05:45:43 -0600 (Sat, 29 Apr 2006) $
  */
-public final class LexTest extends TestCase {
+public final class TestLexer extends TestCase {
     public void testLexer() throws Exception {
         Hashtable tokenTypes = new Hashtable();
         tokenTypes.put(new Integer(TokenType.END_OF_QUERY), "END_OF_QUERY");
