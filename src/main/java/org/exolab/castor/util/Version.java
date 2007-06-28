0c6a74b536291ba3a2d0bf52185a0cd963e20002
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2007-06-28 18:01:07 +0000

    Pre-release preparations fro 1.1.2.1

--- src/main/java/org/exolab/castor/util/Version.java
+++ src/main/java/org/exolab/castor/util/Version.java
@@ -67,12 +67,12 @@ public final class Version {
     /**
      * The version number
      */
-    public static final String VERSION = "1.1.2";
+    public static final String VERSION = "1.1.2.1";
         
     /**
      * The version date.
      */
-    public static final String VERSION_DATE  = "20070617";
+    public static final String VERSION_DATE  = "20070628";
     
     /**
      * The version number with build information
