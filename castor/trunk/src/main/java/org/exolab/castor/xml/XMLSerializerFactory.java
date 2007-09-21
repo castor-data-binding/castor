27adffb0c2f74e73bf7b522f05ef59597ebbb361
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2007-09-21 20:44:12 +0000

    Added @since tag.

--- castor/trunk/src/main/java/org/exolab/castor/xml/XMLSerializerFactory.java
+++ castor/trunk/src/main/java/org/exolab/castor/xml/XMLSerializerFactory.java
@@ -20,6 +20,8 @@ package org.exolab.castor.xml;
  * 
  * @author <a href="mailto:werner DOT guttmann AT gmx DOT net">Werner Guttmann</a>
  * @version $Revision$ $Date: 2006-04-25 16:09:10 -0600 (Tue, 25 Apr 2006) $
+ * 
+ * @since 1.0 
  */
 public interface XMLSerializerFactory {
 	/**
