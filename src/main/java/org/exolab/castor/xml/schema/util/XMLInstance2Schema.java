337816aade1be437d7183f839d331d4e4fc4bf52
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2007-10-29 09:36:21 +0000

    Added Ant task definition for XML instance to schema tool.

--- src/main/java/org/exolab/castor/xml/schema/util/XMLInstance2Schema.java
+++ src/main/java/org/exolab/castor/xml/schema/util/XMLInstance2Schema.java
@@ -51,136 +51,156 @@ import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.Writer;
 
+import org.castor.xml.AbstractInternalContext;
 import org.castor.xml.BackwardCompatibilityContext;
 import org.castor.xml.InternalContext;
-import org.castor.xml.AbstractInternalContext;
 import org.exolab.castor.util.NestedIOException;
 import org.exolab.castor.xml.schema.Order;
 import org.exolab.castor.xml.schema.Schema;
 import org.exolab.castor.xml.schema.writer.SchemaWriter;
 import org.xml.sax.InputSource;
 import org.xml.sax.Parser;
+import org.xml.sax.SAXException;
 
 /**
- * A class for reading XML Schemas
+ * A class for reading XML Schemas.
+ * 
+ * To generate an XML schema from a given XML document instance and write it to 
+ * a file, please use code similar to the following:
  *
+ * InputSource inputSource = ...;
+ * XMLInstance2Schema xi2s = new XMLInstance2Schema();
+ * Schema schema = xi2s.createSchema(inputSource);
+ *
+ * Writer dstWriter = new FileWriter(...);
+ * xi2s.serializeSchema(dstWriter, schema);
+ * dstWriter.close();
+ * 
  * @author <a href="mailto:kvisco@intalio.com">Keith Visco</a>
- * @version $Revision$ $Date: 2006-01-16 13:22:58 -0700 (Mon, 16 Jan 2006) $
-**/
+ * @version $Revision$ $Date: 2006-01-16 13:22:58 -0700 (Mon, 16 Jan
+ *          2006) $
+ */
 public class XMLInstance2Schema {
-    
-    /** 
+
+    /**
      * The {@link AbstractInternalContext} used to get Parser from.
      */
     private InternalContext _internalContext;
-    
-    private Order       _defaultGroup = Order.seq;
-    
+
+    private Order _defaultGroup = Order.seq;
+
     /**
      * Creates a new XMLInstance2Schema
-     *
-    **/
+     * 
+     */
     public XMLInstance2Schema() {
         super();
         _internalContext = new BackwardCompatibilityContext();
-    } //-- XMLInstance2Schema
+    }
 
     /**
-     * Creates an XML Schema using the given XML instance filename. 
-     * The XML Schema created will be based on the specific XML 
-     * instance document.
-     *
-     * @param filename the filename for the XML document
-    **/
-    public Schema createSchema(String filename) 
-        throws IOException
-    {
+     * Creates an XML Schema using the given XML instance filename. The XML
+     * Schema created will be based on the specific XML instance document.
+     * 
+     * @param filename
+     *            the filename for the XML document
+     */
+    public Schema createSchema(String filename) throws IOException {
         return createSchema(new InputSource(filename));
-    } //-- createSchema
-    
+    }
+
     /**
-     * Creates an XML Schema using the given Reader. The reader 
-     * must be for an XML instance document. The XML Schema 
-     * created will be based on the specific XML instance document.
-     *
-     * @param reader the Reader for the XML document
-    **/
-    public Schema createSchema(Reader reader) 
-        throws IOException
-    {
+     * Creates an XML Schema using the given Reader. The reader must be for an
+     * XML instance document. The XML Schema created will be based on the
+     * specific XML instance document.
+     * 
+     * @param reader
+     *            the Reader for the XML document
+     */
+    public Schema createSchema(Reader reader) throws IOException {
         return createSchema(new InputSource(reader));
-    } //-- createSchema
-    
+    }
+
     /**
-     * Creates an XML Schema using the given InputSource. The InputSource
-     * must be for an XML instance document. The XML Schema created will 
-     * be based on the specific XML instance document.
-     *
-     * @param source the InputSource for the XML document
-    **/
-    public Schema createSchema(InputSource source) 
-        throws IOException
-    {
+     * Creates an XML Schema using the given InputSource. The InputSource must
+     * be for an XML instance document. The XML Schema created will be based on
+     * the specific XML instance document.
+     * 
+     * @param source
+     *            the InputSource for the XML document
+     */
+    public Schema createSchema(InputSource source) throws IOException {
         XMLInstance2SchemaHandler handler = new XMLInstance2SchemaHandler();
         handler.setDefaultGroupOrder(_defaultGroup);
-        
+
         try {
-	        Parser parser = _internalContext.getParser();
+            Parser parser = _internalContext.getParser();
             if (parser == null) {
-                throw new IOException("fatal error: unable to create SAX parser.");
+                throw new IOException(
+                        "fatal error: unable to create SAX parser.");
             }
             parser.setDocumentHandler(handler);
             parser.setErrorHandler(handler);
             parser.parse(source);
-        }
-        catch(org.xml.sax.SAXException sx) {
+        } catch (org.xml.sax.SAXException sx) {
             throw new NestedIOException(sx);
         }
         return handler.getSchema();
-    } //-- createSchema
-    
+    }
+
     /**
-     * Sets the default grouping as "all". By default groups
-     * will be treated as "sequence".
-    **/
+     * Sets the default grouping as "all". By default groups will be treated as
+     * "sequence".
+     */
     public void setDefaultGroupingAsAll() {
         _defaultGroup = Order.all;
-    } //-- method: setDefaultGroupAsAll
-    
-    
+    }
+
     /**
-     * For testing purposes only
-    **/
+     * Serializes a {@link Schema} instance to the given {@link Writer} instance.
+     * @param dstWriter The {@link Writer} instance to output the XML schema to.
+     * @param schema The XML {@link Schema} instance to be output.
+     * @throws IOException If there's a problem related to writing to the given {@link Writer} instance. 
+     * @throws SAXException If there's a problem related to SAX streaming.
+     */
+    public void serializeSchema(Writer dstWriter, Schema schema)
+            throws IOException, SAXException {
+        SchemaWriter schemaWriter = new SchemaWriter(dstWriter);
+        schemaWriter.write(schema);
+    }
+
+    /**
+     * For testing purposes only.
+     * @deprecate Please see class documentation for an example of how to use
+     * this class. Or consider using the Ant task for the schema generator.
+     */
     public static void main(String args[]) {
-        
+
         if (args.length == 0) {
             System.out.println("Missing filename");
             System.out.println();
-            System.out.println("usage: java XMLInstance2Schema <input-file> [<output-file> (optional)]");
+            System.out
+                    .println("usage: java XMLInstance2Schema <input-file> [<output-file> (optional)]");
             return;
         }
-        
+
         try {
             XMLInstance2Schema xi2s = new XMLInstance2Schema();
             Schema schema = xi2s.createSchema(args[0]);
-            
+
             Writer dstWriter = null;
-            SchemaWriter schemaWriter = null;
             if (args.length > 1) {
                 dstWriter = new FileWriter(args[1]);
-            }
-            else {
+            } else {
                 dstWriter = new PrintWriter(System.out, true);
             }
-            schemaWriter = new SchemaWriter(dstWriter);
-            schemaWriter.write(schema);
+
+            xi2s.serializeSchema(dstWriter, schema);
             dstWriter.flush();
-        }
-        catch(Exception ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
 
-    } //-- method: main
-    
-    /* */
-} //-- XMLInstance2Schema
+    }
+
+}
