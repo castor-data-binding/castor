2c992ff0ba4f149ff3479d1de21263dd1ab6414f
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2008-03-20 14:48:13 +0000

    Added two missing JAR references to CLASSPATH.

--- castor/trunk/bin/CTFRunFromJAR.bat
+++ castor/trunk/bin/CTFRunFromJAR.bat
@@ -4,8 +4,9 @@ set JAVA=%JAVA_HOME%\bin\java
 set OLDCP=%CLASSPATH%
 set CLASSPATH=..\build\tests;%JAVA_HOME%\lib\tools.jar
 set cp=%CLASSPATH%
-for %%i in (..\dist\castor-*-xmlctf.jar) do call cp.bat %%i
+for %%i in (..\dist\castor-*-xmlctf-framework.jar) do call cp.bat %%i
 for %%i in (..\dist\castor-*-xml.jar) do call cp.bat %%i
+for %%i in (..\dist\castor-*-xml-schema.jar) do call cp.bat %%i
 for %%i in (..\dist\castor-*-codegen.jar) do call cp.bat %%i
 for %%i in (..\lib\*.jar) do call cp.bat %%i
 rmdir /q /s ..\build\tests\output>nul
