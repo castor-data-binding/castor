99a98599101aa7255966fae25fbbae59e4e03646
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2008-04-19 08:17:05 +0000

    Refactored test case templates to use XMLContext and to setup this instance in setUp().

--- castor/trunk/src/bugs/xml/template/TestTemplateWithMapping.java
+++ castor/trunk/src/bugs/xml/template/TestTemplateWithMapping.java
@@ -7,19 +7,27 @@ import junit.framework.TestCase;
 import org.exolab.castor.mapping.Mapping;
 import org.exolab.castor.xml.Marshaller;
 import org.exolab.castor.xml.Unmarshaller;
+import org.exolab.castor.xml.XMLContext;
 import org.xml.sax.InputSource;
 
 public final class TestTemplateWithMapping extends TestCase {
     
     private static final String SAMPLE_FILE = "input.xml";
     private static final String MAPPING_FILE = "mapping.xml";
-    
+
+    private XMLContext context;
+
     public TestTemplateWithMapping() {
         super();
     }
     
     public TestTemplateWithMapping(final String name) {
         super(name);
+    }
+
+    protected void setUp() throws Exception {
+        this.context = new XMLContext();
+        context.setProperty("org.exolab.castor.indent", "true");
     }
 
     /**
@@ -30,7 +38,8 @@ public final class TestTemplateWithMapping extends TestCase {
         Mapping mapping = new Mapping();
         mapping.loadMapping(getClass().getResource(MAPPING_FILE).toExternalForm());
         
-        Unmarshaller unmarshaller = new Unmarshaller (Entity.class);
+        Unmarshaller unmarshaller = getXMLContext().createUnmarshaller();
+        unmarshaller.setClass(Entity.class);
         unmarshaller.setMapping(mapping);
         
         Entity entity = (Entity) unmarshaller.unmarshal(new InputSource(getClass().getResource(SAMPLE_FILE).toExternalForm()));
@@ -48,7 +57,8 @@ public final class TestTemplateWithMapping extends TestCase {
         mapping.loadMapping(getClass().getResource(MAPPING_FILE).toExternalForm());
         
         StringWriter out = new StringWriter();
-        Marshaller marshaller = new Marshaller (out);
+        Marshaller marshaller = getXMLContext().createMarshaller();
+        marshaller.setWriter(out);
         marshaller.setMapping(mapping);
         
         Entity entity = new Entity();
@@ -58,6 +68,10 @@ public final class TestTemplateWithMapping extends TestCase {
         marshaller.marshal(entity);
         
         System.out.println(out.toString());
+    }
+    
+    private XMLContext getXMLContext() {
+        return this.context;
     }
     
 }
