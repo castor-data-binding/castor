1cc16317bf4ab2afdde0e27ef2ba26daaaba84c1
rjoachim <rjoachim@b24b0d9a-6811-0410-802a-946fa971d308>
2008-08-16 10:07:04 +0000

    Creating the JavaCC Parser Generator for Reduced EJB QL (CASTOR-2433 / part 1)

---
+++ cpa/src/main/java/org/castor/cpa/query/ejbql/EjbQLParserAdapter.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2008 Udai Gupta, Ralf Joachim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.castor.cpa.query.ejbql;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+import org.castor.cpa.query.AbstractParser;
+import org.castor.cpa.query.ParseException;
+import org.castor.cpa.query.TokenManagerError;
+import org.castor.cpa.query.QueryObject;
+
+/**
+ * Class that implements abstract parser generator.
+ * 
+ * @author <a href="mailto:mailtoud AT gmail DOT com">Udai Gupta</a>
+ * @author <a href="mailto:ralf DOT joachim AT syscon DOT eu">Ralf Joachim</a>
+ * @version $Revision: 7121 $ $Date: 2006-04-25 16:09:10 -0600 (Tue, 25 Apr 2006) $
+ * @since 1.3
+ */
+public final class EjbQLParserAdapter extends AbstractParser {
+    // --------------------------------------------------------------------------
+
+    /**
+     * {@inheritDoc}
+     * 
+     * @throws UnsupportedEncodingException
+     * @throws ParseException
+     */
+    public QueryObject parse(final String oql)
+            throws UnsupportedEncodingException, ParseException {
+        EjbQLParser parser = null;
+        EjbQLParserTokenManager tkmgr = null;
+        try {
+            tkmgr = createTkmgr(oql);
+            parser = new EjbQLParser(tkmgr);
+            SimpleNode root = parser.select_statement();
+            EjbQLTreeWalker tw = new EjbQLTreeWalker(root);
+            return tw.getSelect();
+        } catch (org.castor.cpa.query.ejbql.ParseException e) {
+            parser.ReInit(tkmgr);
+            throw new ParseException(e);
+        } catch (org.castor.cpa.query.castorql.TokenMgrError e) {
+            // parser.ReInit(tkmgr);
+            throw new TokenManagerError(e);
+        }
+    }
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Gets the root SimpleNode instance of JJTREE.
+     * 
+     * @param oql the query string
+     * @return the root SimpleNode of JJTREE 
+     * @throws UnsupportedEncodingException
+     *                 the unsupported encoding exception
+     * @throws ParseException
+     *                 the QL parse exception
+     */
+    public SimpleNode getSimpleNode(final String oql)
+            throws UnsupportedEncodingException, ParseException {
+        EjbQLParser parser = null;
+        EjbQLParserTokenManager tkmgr = null;
+        try {
+            tkmgr = createTkmgr(oql);
+            parser = new EjbQLParser(tkmgr);
+            return parser.select_statement();
+        } catch (org.castor.cpa.query.ejbql.ParseException e) {
+            parser.ReInit(tkmgr);
+            throw new ParseException(e);
+        } catch (org.castor.cpa.query.castorql.TokenMgrError e) {
+            // parser.ReInit(tkmgr);
+            throw new TokenManagerError(e);
+        }
+    }
+    
+    // --------------------------------------------------------------------------
+
+    /**
+     * Creates the TokenManager instance.
+     * 
+     * @param oql the query string
+     * @return the castor ql parser token manager
+     * @throws UnsupportedEncodingException
+     *                 the unsupported encoding exception
+     */
+    public EjbQLParserTokenManager createTkmgr(final String oql)
+            throws UnsupportedEncodingException {
+        StringBuffer stringBuffer = new StringBuffer(oql);
+        InputStream bis = new ByteArrayInputStream(stringBuffer.toString()
+                .getBytes("UTF-8"));
+        InputStreamReader isr = new InputStreamReader(bis, "UTF-8");
+        SimpleCharStream jSt = new SimpleCharStream(isr);
+        return new EjbQLParserTokenManager(jSt);
+    }
+
+    // --------------------------------------------------------------------------
+}
---
+++ cpa/src/test/java/org/castor/cpa/query/ejbql/TestEjbQLParser.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2008 Udai Gupta, Ralf Joachim
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.castor.cpa.query.ejbql;
+
+import junit.framework.TestCase;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Junit test for analyzing the performance of CastorQL Parser created by JavaCC
+ * 4.0 for reduced EJBQL for more information refer to the issue CASTOR-2396
+ * 
+ * @author <a href="mailto:mailtoud AT gmail DOT com">Udai Gupta</a>
+ * @author <a href="mailto:ralf DOT joachim AT syscon DOT eu">Ralf Joachim</a>
+ * @version $Revision: 7121 $ $Date: 2006-04-25 16:09:10 -0600 (Tue, 25 Apr
+ *          2006) $
+ * @since 1.3
+ */
+public class TestEjbQLParser extends TestCase {
+
+    // --------------------------------------------------------------------------
+    public TestEjbQLParser(final String name) {
+        super(name);
+    }
+
+    // --------------------------------------------------------------------------
+    public static void testParserPerformance() {
+
+        System.out.println("start : Ejb");
+
+        // Modify to test more
+        String oql = "Select DisTinct o.item "
+                + "from de.jsci.pcv.jdo.LieferantJDO as o "
+                + "where o.deleted = false " + "order by o.name, o.id desc "
+                + "limit ?1, :2";
+
+        long start = 0L;
+
+        // for(int i=0;i<10000;i++){
+
+        start = System.nanoTime();
+        runParser(oql);
+
+        // }
+        long stop = System.nanoTime();
+
+        System.out.println("end : Ejb Parsed :" + oql);
+
+        System.out.println(" in " + (stop - start) / 1000000.0
+                + " milliseconds");
+
+    }
+
+    // --------------------------------------------------------------------------
+    /**
+     * Method which passes the OQL String to CastorQL Parser
+     */
+    public static void runParser(final String oql) {
+
+        StringBuffer stringBuffer = new StringBuffer(oql);
+
+        EjbQLParser parser = null;
+        try {
+
+            InputStream bis = new ByteArrayInputStream(stringBuffer.toString()
+                    .getBytes("UTF-8"));
+
+            InputStreamReader isr = new InputStreamReader(bis, "UTF-8");
+
+            SimpleCharStream jSt = new SimpleCharStream(isr);
+
+            EjbQLParserTokenManager tkmgr = new EjbQLParserTokenManager(
+                    jSt);
+
+            parser = new EjbQLParser(tkmgr);
+
+            parser.select_statement();
+
+        } catch (Exception e) {
+
+            System.out.println("NOK.");
+            System.out.println(e.getMessage());
+            parser.ReInit(System.in);
+
+        } catch (Error e) {
+            e.printStackTrace();
+        }
+    }
+    // --------------------------------------------------------------------------
+}
