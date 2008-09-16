935d3173a76b1ce4cb14595a485096d5368abbe2
rjoachim <rjoachim@b24b0d9a-6811-0410-802a-946fa971d308>
2008-09-16 17:41:50 +0000

    Query returns entities of wrong type for extended class hierarchies (CASTOR-2527)

---
+++ cpactf/src/test/ddl/org/castor/cpa/test/test2527/mysql.sql
@@ -0,0 +1,70 @@
+DROP TABLE IF EXISTS TEST2527_LOG_REFERENCE;
+DROP TABLE IF EXISTS TEST2527_LOG_EXCEPTION;
+DROP TABLE IF EXISTS TEST2527_LOG;
+
+
+CREATE TABLE TEST2527_LOG (
+            ID         INT NOT NULL,
+            STAMP      TIMESTAMP NOT NULL,
+            SOURCE     VARCHAR(100) NOT NULL,  
+            LEVEL      VARCHAR(10) NOT NULL,  
+            MESSAGE    VARCHAR(500) NULL
+);  
+
+ALTER TABLE TEST2527_LOG ADD PRIMARY KEY (ID);
+
+
+CREATE TABLE TEST2527_LOG_EXCEPTION (
+            ID         INT NOT NULL,
+            ENTRY_ID   INT NOT NULL,
+            STACKTRACE VARCHAR(2000) NOT NULL
+);
+
+ALTER TABLE TEST2527_LOG_EXCEPTION ADD PRIMARY KEY (ID);
+
+ALTER TABLE TEST2527_LOG_EXCEPTION
+ADD CONSTRAINT TEST2527_LOG_EXCEPTION_FK
+FOREIGN KEY (ENTRY_ID) REFERENCES TEST2527_LOG (ID);
+
+
+CREATE TABLE TEST2527_LOG_REFERENCE (
+            ID         INT NOT NULL,
+            TYPE       VARCHAR(100) NOT NULL,
+            VALUE      VARCHAR(100) NOT NULL
+);
+
+ALTER TABLE TEST2527_LOG_REFERENCE ADD PRIMARY KEY (ID);
+
+ALTER TABLE TEST2527_LOG_REFERENCE
+ADD CONSTRAINT TEST2527_LOG_REFERENZ_FK
+FOREIGN KEY (ID) REFERENCES TEST2527_LOG (ID);
+
+
+INSERT INTO TEST2527_LOG VALUES (1, '2008-12-31 12:34:56', 'test1', 'level', 'simple log entry 1');
+INSERT INTO TEST2527_LOG VALUES (2, '2008-12-31 12:34:56', 'test1', 'level', 'simple log entry 2');
+
+INSERT INTO TEST2527_LOG VALUES (3, '2008-12-31 12:34:56', 'test2', 'level', 'exception log entry 1');
+INSERT INTO TEST2527_LOG_EXCEPTION VALUES (1, 3, 'stacktrace for exception log entry 1');
+INSERT INTO TEST2527_LOG VALUES (4, '2008-12-31 12:34:56', 'test2', 'level', 'exception log entry 2');
+INSERT INTO TEST2527_LOG_EXCEPTION VALUES (2, 4, 'stacktrace for exception log entry 2');
+
+INSERT INTO TEST2527_LOG VALUES (5, '2008-12-31 12:34:56', 'test3', 'level', 'refering log entry 1');
+INSERT INTO TEST2527_LOG_REFERENCE VALUES (5, 'type', 'ref 1');
+INSERT INTO TEST2527_LOG VALUES (6, '2008-12-31 12:34:56', 'test3', 'level', 'refering log entry 2');
+INSERT INTO TEST2527_LOG_REFERENCE VALUES (6, 'type', 'ref 2');
+
+INSERT INTO TEST2527_LOG VALUES (7, '2008-12-31 12:34:56', 'test4', 'level', 'simple and exception log entry 1');
+INSERT INTO TEST2527_LOG VALUES (8, '2008-12-31 12:34:56', 'test4', 'level', 'simple and exception log entry 2');
+INSERT INTO TEST2527_LOG_EXCEPTION VALUES (3, 8, 'stacktrace for simple and exception log entry 2');
+INSERT INTO TEST2527_LOG VALUES (9, '2008-12-31 12:34:56', 'test4', 'level', 'simple and exception log entry 3');
+
+INSERT INTO TEST2527_LOG VALUES (10, '2008-12-31 12:34:56', 'test5', 'level', 'simple and refering log entry 1');
+INSERT INTO TEST2527_LOG VALUES (11, '2008-12-31 12:34:56', 'test5', 'level', 'simple and refering log entry 2');
+INSERT INTO TEST2527_LOG_REFERENCE VALUES (11, 'type', 's+r 2');
+INSERT INTO TEST2527_LOG VALUES (12, '2008-12-31 12:34:56', 'test5', 'level', 'simple and refering log entry 3');
+
+INSERT INTO TEST2527_LOG VALUES (13, '2008-12-31 12:34:56', 'test6', 'level', 'exception, refering and simple log entry 1');
+INSERT INTO TEST2527_LOG_EXCEPTION VALUES (4, 13, 'stacktrace for exception, refering and simple log entry 1');
+INSERT INTO TEST2527_LOG VALUES (14, '2008-12-31 12:34:56', 'test6', 'level', 'exception, refering and simple log entry 2');
+INSERT INTO TEST2527_LOG_REFERENCE VALUES (14, 'type', 'e+r+s 2');
+INSERT INTO TEST2527_LOG VALUES (15, '2008-12-31 12:34:56', 'test6', 'level', 'exception, refering and simple log entry 3');
---
+++ cpactf/src/test/java/org/castor/cpa/test/test2527/Test2527.java
@@ -0,0 +1,89 @@
+package org.castor.cpa.test.test2527;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.castor.cpa.test.framework.CPATestCase;
+import org.castor.cpa.test.framework.xml.types.DatabaseEngineType;
+import org.exolab.castor.jdo.Database;
+import org.exolab.castor.jdo.OQLQuery;
+import org.exolab.castor.jdo.QueryResults;
+
+public final class Test2527 extends CPATestCase {
+    private static final Log LOG = LogFactory.getLog(Test2527.class);
+    
+    private static final String DBNAME = "test2527";
+    private static final String MAPPING = "/org/castor/cpa/test/test2527/mapping.xml";
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite(Test2527.class.getName());
+        
+        suite.addTest(new Test2527("test"));
+
+        return suite;
+    }
+    
+    public Test2527(final String name) {
+        super(name);
+    }
+    
+    public boolean include(final DatabaseEngineType engine) {
+        return (engine == DatabaseEngineType.MYSQL);
+    }
+    
+    public void test() throws Exception {
+        Database db = getJDOManager(DBNAME, MAPPING).getDatabase();
+
+        db.begin();
+        LOG.info("Begin transaction to query log entries");
+
+        OQLQuery oql = db.getOQLQuery("SELECT e FROM " + LogEntry.class.getName() + " e");
+
+        QueryResults results = oql.execute();
+        while (results.hasMore())  {
+            LogEntry entry = (LogEntry) results.next();
+            int id = entry.getId().intValue();
+            boolean isRefering = (entry instanceof ReferingLogEntry);
+            boolean isException = (entry.getException() != null);
+            
+            if (LOG.isDebugEnabled()) {
+                LOG.debug(id + "/" + isRefering + "/" + isException);
+            }
+
+            switch (id) {
+            case 1:
+            case 2:
+            case 7:
+            case 9:
+            case 10:
+            case 12:
+            case 15:
+                assertFalse(isRefering);
+                assertFalse(isException);
+                break;
+            case 3:
+            case 4:
+            case 8:
+            case 13:
+                assertFalse(isRefering);
+                assertTrue(isException);
+                break;
+            case 5:
+            case 6:
+            case 11:
+            case 14:
+                assertTrue(isRefering);
+                assertFalse(isException);
+                break;
+            default:
+                fail();
+            }
+        }
+
+        LOG.info("End transaction to query log entries");
+        db.commit();
+        db.close();
+    }
+}
