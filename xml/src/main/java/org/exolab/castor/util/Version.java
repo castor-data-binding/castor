51cc6c5426a4da48de07d064359be780ce75909b
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2008-09-26 08:26:30 +0000

    Upgraded version number to 1.3rc1.

--- xml/src/main/java/org/exolab/castor/util/Version.java
+++ xml/src/main/java/org/exolab/castor/util/Version.java
@@ -67,12 +67,12 @@ public final class Version {
     /**
      * The version number
      */
-    public static final String VERSION = "1.2";
+    public static final String VERSION = "1.3rc1";
         
     /**
      * The version date.
      */
-    public static final String VERSION_DATE  = "20080204";
+    public static final String VERSION_DATE  = "20081001";
     
     /**
      * The version number with build information
