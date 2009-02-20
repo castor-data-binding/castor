9ce88a8c59f08ccee538aa07d9d763ffd88667c4
rjoachim <rjoachim@b24b0d9a-6811-0410-802a-946fa971d308>
2009-02-20 20:51:51 +0000

    Added test to verify that ObjectNotFoundException during load() releases in-memory read lock (CASTOR-241)

---
+++ castor/trunk/cpactf/src/test/ddl/org/castor/cpa/test/test241/mssql.sql
@@ -0,0 +1,8 @@
+DROP TABLE test241_prod
+GO
+
+CREATE TABLE test241_prod(
+	id int NOT NULL,
+	name varchar(200) NOT NULL
+) 
+GO
---
+++ castor/trunk/cpactf/src/test/ddl/org/castor/cpa/test/test241/mysql.sql
@@ -0,0 +1,6 @@
+drop table if exists test241_prod;
+
+create table test241_prod (
+  id        int           not null,
+  name      varchar(200)  not null
+);
