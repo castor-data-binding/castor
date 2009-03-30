8cd07f7fb88f25ffc12cd4518ee39f5bb4904643
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2009-03-30 08:55:59 +0000

    Fixed problem with new test case.

---
+++ xmlctf/tests/MasterTestSuite/mapping/anyNode/preserveNamespacesWithGlobalNamespaceDef/Resource.java
@@ -0,0 +1,12 @@
+public class Resource {
+
+    private Object elementContent;
+
+    public Object getElementContent() {
+        return elementContent;
+    }
+
+    public void setElementContent(Object elementContent) {
+        this.elementContent = elementContent;
+    }
+}
