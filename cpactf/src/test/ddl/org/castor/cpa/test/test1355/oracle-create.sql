ffcc68d21b7c650fbf597fa6a3d175dfd43bcadf
rjoachim <rjoachim@b24b0d9a-6811-0410-802a-946fa971d308>
2009-06-08 22:34:22 +0000

    Use only lower case table and column names in cpactf tests (CASTOR-2738)

--- cpactf/src/test/ddl/org/castor/cpa/test/test1355/oracle-create.sql
+++ cpactf/src/test/ddl/org/castor/cpa/test/test1355/oracle-create.sql
@@ -1,213 +1,213 @@
 CREATE TABLE test1355_country (
-    ID NUMERIC(18,0) NOT NULL,
-    NAME VARCHAR(100) NOT NULL,
-    ISO3_COUNTRY_CODE VARCHAR(3) ,
-    RECORD_CREATED_BY_ID NUMERIC(18,0) ,
-    RECORD_LAST_MOD_BY_ID NUMERIC(18,0) ,
-    RECORD_CREATE_DATE DATE ,
-    RECORD_LAST_MOD_DATE DATE
+    id NUMERIC(18,0) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    iso3_country_code VARCHAR(3) ,
+    record_created_by_id NUMERIC(18,0) ,
+    record_last_mod_by_id NUMERIC(18,0) ,
+    record_create_date DATE ,
+    record_last_mod_date DATE
 )
 /
 
 CREATE TABLE test1355_state_prov (
-    ID NUMERIC(18,0) NOT NULL,
-    CODE VARCHAR(3) ,
-    NAME VARCHAR(100) NOT NULL,
-    COUNTRY_ID NUMERIC(18,0) NOT NULL,
-    RECORD_CREATED_BY_ID NUMERIC(18,0) ,
-    RECORD_LAST_MOD_BY_ID NUMERIC(18,0) ,
-    RECORD_CREATE_DATE DATE ,
-    RECORD_LAST_MOD_DATE DATE
+    id NUMERIC(18,0) NOT NULL,
+    code VARCHAR(3) ,
+    name VARCHAR(100) NOT NULL,
+    country_id NUMERIC(18,0) NOT NULL,
+    record_created_by_id NUMERIC(18,0) ,
+    record_last_mod_by_id NUMERIC(18,0) ,
+    record_create_date DATE ,
+    record_last_mod_date DATE
 )
 /
 
 CREATE TABLE test1355_city (
-    ID NUMERIC(18,0) NOT NULL,
-    NAME VARCHAR(100) NOT NULL,
-    STATE_PROV_ID NUMERIC(18,0) NOT NULL,
-    RECORD_CREATED_BY_ID NUMERIC(18,0) ,
-    RECORD_LAST_MOD_BY_ID NUMERIC(18,0) ,
-    RECORD_CREATE_DATE DATE ,
-    RECORD_LAST_MOD_DATE DATE
+    id NUMERIC(18,0) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    state_prov_id NUMERIC(18,0) NOT NULL,
+    record_created_by_id NUMERIC(18,0) ,
+    record_last_mod_by_id NUMERIC(18,0) ,
+    record_create_date DATE ,
+    record_last_mod_date DATE
 )
 /
 
 CREATE TABLE test1355_golf_course (
-    ID NUMERIC(18,0) NOT NULL,
-    COURSE_NAME VARCHAR(100) NOT NULL,
-    HOLES INTEGER NOT NULL,
-    CITY_ID NUMERIC(18,0) ,
-    RECORD_CREATED_BY_ID NUMERIC(18,0) ,
-    RECORD_LAST_MOD_BY_ID NUMERIC(18,0) ,
-    RECORD_CREATE_DATE DATE ,
-    RECORD_LAST_MOD_DATE DATE
+    id NUMERIC(18,0) NOT NULL,
+    course_name VARCHAR(100) NOT NULL,
+    holes INTEGER NOT NULL,
+    city_id NUMERIC(18,0) ,
+    record_created_by_id NUMERIC(18,0) ,
+    record_last_mod_by_id NUMERIC(18,0) ,
+    record_create_date DATE ,
+    record_last_mod_date DATE
 )
 /
 
 CREATE TABLE test1355_golf_course_tees (
-    ID NUMERIC(18,0) NOT NULL,
-    COURSE_ID NUMERIC(18,0) NOT NULL,
-    TEES_NAME VARCHAR(100) NOT NULL,
-    TEES_COLOR VARCHAR(25) ,
-    MENS_SLOPE INTEGER ,
-    MENS_RATING DOUBLE PRECISION ,
-    WOMENS_SLOPE INTEGER ,
-    WOMENS_RATING DOUBLE PRECISION ,
-    YARDAGE INTEGER ,
-    RECORD_CREATED_BY_ID NUMERIC(18,0) ,
-    RECORD_LAST_MOD_BY_ID NUMERIC(18,0) ,
-    RECORD_CREATE_DATE DATE ,
-    RECORD_LAST_MOD_DATE DATE
+    id NUMERIC(18,0) NOT NULL,
+    course_id NUMERIC(18,0) NOT NULL,
+    tees_name VARCHAR(100) NOT NULL,
+    tees_color VARCHAR(25) ,
+    mens_slope INTEGER ,
+    mens_rating DOUBLE PRECISION ,
+    womens_slope INTEGER ,
+    womens_rating DOUBLE PRECISION ,
+    yardage INTEGER ,
+    record_created_by_id NUMERIC(18,0) ,
+    record_last_mod_by_id NUMERIC(18,0) ,
+    record_create_date DATE ,
+    record_last_mod_date DATE
 )
 /
 
 CREATE TABLE test1355_golf_course_holes (
-    ID NUMERIC(18,0) NOT NULL,
-    COURSE_TEES_ID NUMERIC(18,0) NOT NULL,
-    NUM INTEGER NOT NULL,
-    HOLE_NAME VARCHAR(100) ,
-    PAR INTEGER ,
-    YARDAGE INTEGER ,
-    RECORD_CREATED_BY_ID NUMERIC(18,0) ,
-    RECORD_LAST_MOD_BY_ID NUMERIC(18,0) ,
-    RECORD_CREATE_DATE DATE ,
-    RECORD_LAST_MOD_DATE DATE
+    id NUMERIC(18,0) NOT NULL,
+    course_tees_id NUMERIC(18,0) NOT NULL,
+    num INTEGER NOT NULL,
+    hole_name VARCHAR(100) ,
+    par INTEGER ,
+    yardage INTEGER ,
+    record_created_by_id NUMERIC(18,0) ,
+    record_last_mod_by_id NUMERIC(18,0) ,
+    record_create_date DATE ,
+    record_last_mod_date DATE
 )
 /
 
-insert into test1355_country (ID, NAME, ISO3_COUNTRY_CODE) values ('1', 'USA', 'USA')
+insert into test1355_country (id, name, iso3_country_code) values ('1', 'USA', 'USA')
 /
-insert into test1355_state_prov (ID, CODE, NAME, COUNTRY_ID) values ('1', 'WA', 'Washington', '1')
+insert into test1355_state_prov (id, code, name, country_id) values ('1', 'WA', 'Washington', '1')
 /
-insert into test1355_city (ID, NAME, STATE_PROV_ID) values ('1', 'Seattle', '1')
-/
-
-insert into test1355_golf_course (ID, COURSE_NAME, HOLES, CITY_ID) values ('1', 'West Seattle', '18', '1')
-/
-insert into test1355_golf_course_tees (ID, COURSE_ID, TEES_NAME, TEES_COLOR) values ('1', '1', 'White', 'white')
-/
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('1', '1', '1', '4')
-/
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('2', '1', '2', '4')
-/
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('3', '1', '3', '4')
-/
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('4', '1', '4', '4')
-/
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('5', '1', '5', '4')
-/
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('6', '1', '6', '4')
-/
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('7', '1', '7', '4')
-/
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('8', '1', '8', '4')
-/
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('9', '1', '9', '4')
+insert into test1355_city (id, name, state_prov_id) values ('1', 'Seattle', '1')
 /
 
-insert into test1355_golf_course_tees (ID, COURSE_ID, TEES_NAME, TEES_COLOR) values ('2', '1', 'Blue', 'blue')
+insert into test1355_golf_course (id, course_name, holes, city_id) values ('1', 'West Seattle', '18', '1')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('10', '2', '1', '4')
+insert into test1355_golf_course_tees (id, course_id, tees_name, tees_color) values ('1', '1', 'White', 'white')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('11', '2', '2', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('1', '1', '1', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('12', '2', '3', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('2', '1', '2', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('13', '2', '4', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('3', '1', '3', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('14', '2', '5', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('4', '1', '4', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('15', '2', '6', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('5', '1', '5', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('16', '2', '7', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('6', '1', '6', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('17', '2', '8', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('7', '1', '7', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('18', '2', '9', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('8', '1', '8', '4')
 /
-
-insert into test1355_golf_course_tees (ID, COURSE_ID, TEES_NAME, TEES_COLOR) values ('3', '1', 'Red', 'red')
-/
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('19', '3', '1', '4')
-/
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('20', '3', '2', '4')
-/
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('21', '3', '3', '4')
-/
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('22', '3', '4', '4')
-/
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('23', '3', '5', '4')
-/
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('24', '3', '6', '4')
-/
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('25', '3', '7', '4')
-/
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('26', '3', '8', '4')
-/
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('27', '3', '9', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('9', '1', '9', '4')
 /
 
-insert into test1355_golf_course (ID, COURSE_NAME, HOLES, CITY_ID) values ('2', 'Jackson Park', '18', '1')
+insert into test1355_golf_course_tees (id, course_id, tees_name, tees_color) values ('2', '1', 'Blue', 'blue')
 /
-insert into test1355_golf_course_tees (ID, COURSE_ID, TEES_NAME, TEES_COLOR) values ('4', '2', 'White', 'white')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('10', '2', '1', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('28', '4', '1', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('11', '2', '2', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('29', '4', '2', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('12', '2', '3', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('30', '4', '3', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('13', '2', '4', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('31', '4', '4', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('14', '2', '5', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('32', '4', '5', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('15', '2', '6', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('33', '4', '6', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('16', '2', '7', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('34', '4', '7', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('17', '2', '8', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('35', '4', '8', '4')
-/
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('36', '4', '9', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('18', '2', '9', '4')
 /
 
-insert into test1355_golf_course_tees (ID, COURSE_ID, TEES_NAME, TEES_COLOR) values ('5', '2', 'Blue', 'blue')
+insert into test1355_golf_course_tees (id, course_id, tees_name, tees_color) values ('3', '1', 'Red', 'red')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('37', '5', '1', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('19', '3', '1', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('38', '5', '2', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('20', '3', '2', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('39', '5', '3', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('21', '3', '3', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('40', '5', '4', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('22', '3', '4', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('41', '5', '5', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('23', '3', '5', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('42', '5', '6', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('24', '3', '6', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('43', '5', '7', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('25', '3', '7', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('44', '5', '8', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('26', '3', '8', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('45', '5', '9', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('27', '3', '9', '4')
 /
 
-insert into test1355_golf_course_tees (ID, COURSE_ID, TEES_NAME, TEES_COLOR) values ('6', '2', 'Red', 'red')
+insert into test1355_golf_course (id, course_name, holes, city_id) values ('2', 'Jackson Park', '18', '1')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('46', '6', '1', '4')
+insert into test1355_golf_course_tees (id, course_id, tees_name, tees_color) values ('4', '2', 'White', 'white')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('47', '6', '2', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('28', '4', '1', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('48', '6', '3', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('29', '4', '2', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('49', '6', '4', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('30', '4', '3', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('50', '6', '5', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('31', '4', '4', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('51', '6', '6', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('32', '4', '5', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('52', '6', '7', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('33', '4', '6', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('53', '6', '8', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('34', '4', '7', '4')
 /
-insert into test1355_golf_course_holes (ID, COURSE_TEES_ID, NUM, PAR) values ('54', '6', '9', '4')
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('35', '4', '8', '4')
+/
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('36', '4', '9', '4')
+/
+
+insert into test1355_golf_course_tees (id, course_id, tees_name, tees_color) values ('5', '2', 'Blue', 'blue')
+/
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('37', '5', '1', '4')
+/
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('38', '5', '2', '4')
+/
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('39', '5', '3', '4')
+/
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('40', '5', '4', '4')
+/
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('41', '5', '5', '4')
+/
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('42', '5', '6', '4')
+/
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('43', '5', '7', '4')
+/
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('44', '5', '8', '4')
+/
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('45', '5', '9', '4')
+/
+
+insert into test1355_golf_course_tees (id, course_id, tees_name, tees_color) values ('6', '2', 'Red', 'red')
+/
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('46', '6', '1', '4')
+/
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('47', '6', '2', '4')
+/
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('48', '6', '3', '4')
+/
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('49', '6', '4', '4')
+/
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('50', '6', '5', '4')
+/
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('51', '6', '6', '4')
+/
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('52', '6', '7', '4')
+/
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('53', '6', '8', '4')
+/
+insert into test1355_golf_course_holes (id, course_tees_id, num, par) values ('54', '6', '9', '4')
 /
--- cpactf/src/test/ddl/org/castor/cpa/test/test2527/mysql-create.sql
+++ cpactf/src/test/ddl/org/castor/cpa/test/test2527/mysql-create.sql
@@ -1,65 +1,65 @@
-CREATE TABLE TEST2527_LOG (
-            ID         INT NOT NULL,
-            TIME_STAMP      TIMESTAMP NOT NULL,
-            SOURCE     VARCHAR(100) NOT NULL,  
-            LLEVEL     VARCHAR(10) NOT NULL,  
-            MESSAGE    VARCHAR(500) NULL
+CREATE TABLE test2527_log (
+            id         INT NOT NULL,
+            time_stamp TIMESTAMP NOT NULL,
+            source     VARCHAR(100) NOT NULL,  
+            llevel     VARCHAR(10) NOT NULL,  
+            message    VARCHAR(500) NULL
 );  
 
-ALTER TABLE TEST2527_LOG ADD PRIMARY KEY (ID);
+ALTER TABLE test2527_log ADD PRIMARY KEY (id);
 
 
-CREATE TABLE TEST2527_LOG_EXCEPTION (
-            ID         INT NOT NULL,
-            ENTRY_ID   INT NOT NULL,
-            STACKTRACE VARCHAR(2000) NOT NULL
+CREATE TABLE test2527_log_exception (
+            id         INT NOT NULL,
+            entry_id   INT NOT NULL,
+            stacktrace VARCHAR(2000) NOT NULL
 );
 
-ALTER TABLE TEST2527_LOG_EXCEPTION ADD PRIMARY KEY (ID);
+ALTER TABLE test2527_log_exception ADD PRIMARY KEY (id);
 
-ALTER TABLE TEST2527_LOG_EXCEPTION
-ADD CONSTRAINT TEST2527_LOG_EXCEPTION_FK
-FOREIGN KEY (ENTRY_ID) REFERENCES TEST2527_LOG (ID);
+ALTER TABLE test2527_log_exception
+ADD CONSTRAINT test2527_log_exception_fk
+FOREIGN KEY (entry_id) REFERENCES test2527_log (id);
 
 
-CREATE TABLE TEST2527_LOG_REFERENCE (
-            ID         INT NOT NULL,
-            RTYPE       VARCHAR(100) NOT NULL,
-            RVALUE      VARCHAR(100) NOT NULL
+CREATE TABLE test2527_log_reference (
+            id         INT NOT NULL,
+            rtype      VARCHAR(100) NOT NULL,
+            rvalue     VARCHAR(100) NOT NULL
 );
 
-ALTER TABLE TEST2527_LOG_REFERENCE ADD PRIMARY KEY (ID);
+ALTER TABLE test2527_log_reference ADD PRIMARY KEY (id);
 
-ALTER TABLE TEST2527_LOG_REFERENCE
-ADD CONSTRAINT TEST2527_LOG_REFERENZ_FK
-FOREIGN KEY (ID) REFERENCES TEST2527_LOG (ID);
+ALTER TABLE test2527_log_reference
+ADD CONSTRAINT test2527_log_reference_fk
+FOREIGN KEY (id) REFERENCES test2527_log (id);
 
 
-INSERT INTO TEST2527_LOG VALUES (1, '2008-12-31 12:34:56', 'test1', 'level', 'simple log entry 1');
-INSERT INTO TEST2527_LOG VALUES (2, '2008-12-31 12:34:56', 'test1', 'level', 'simple log entry 2');
+INSERT INTO test2527_log VALUES (1, '2008-12-31 12:34:56', 'test1', 'level', 'simple log entry 1');
+INSERT INTO test2527_log VALUES (2, '2008-12-31 12:34:56', 'test1', 'level', 'simple log entry 2');
 
-INSERT INTO TEST2527_LOG VALUES (3, '2008-12-31 12:34:56', 'test2', 'level', 'exception log entry 1');
-INSERT INTO TEST2527_LOG_EXCEPTION VALUES (1, 3, 'stacktrace for exception log entry 1');
-INSERT INTO TEST2527_LOG VALUES (4, '2008-12-31 12:34:56', 'test2', 'level', 'exception log entry 2');
-INSERT INTO TEST2527_LOG_EXCEPTION VALUES (2, 4, 'stacktrace for exception log entry 2');
+INSERT INTO test2527_log VALUES (3, '2008-12-31 12:34:56', 'test2', 'level', 'exception log entry 1');
+INSERT INTO test2527_log_exception VALUES (1, 3, 'stacktrace for exception log entry 1');
+INSERT INTO test2527_log VALUES (4, '2008-12-31 12:34:56', 'test2', 'level', 'exception log entry 2');
+INSERT INTO test2527_log_exception VALUES (2, 4, 'stacktrace for exception log entry 2');
 
-INSERT INTO TEST2527_LOG VALUES (5, '2008-12-31 12:34:56', 'test3', 'level', 'refering log entry 1');
-INSERT INTO TEST2527_LOG_REFERENCE VALUES (5, 'type', 'ref 1');
-INSERT INTO TEST2527_LOG VALUES (6, '2008-12-31 12:34:56', 'test3', 'level', 'refering log entry 2');
-INSERT INTO TEST2527_LOG_REFERENCE VALUES (6, 'type', 'ref 2');
+INSERT INTO test2527_log VALUES (5, '2008-12-31 12:34:56', 'test3', 'level', 'refering log entry 1');
+INSERT INTO test2527_log_reference VALUES (5, 'type', 'ref 1');
+INSERT INTO test2527_log VALUES (6, '2008-12-31 12:34:56', 'test3', 'level', 'refering log entry 2');
+INSERT INTO test2527_log_reference VALUES (6, 'type', 'ref 2');
 
-INSERT INTO TEST2527_LOG VALUES (7, '2008-12-31 12:34:56', 'test4', 'level', 'simple and exception log entry 1');
-INSERT INTO TEST2527_LOG VALUES (8, '2008-12-31 12:34:56', 'test4', 'level', 'simple and exception log entry 2');
-INSERT INTO TEST2527_LOG_EXCEPTION VALUES (3, 8, 'stacktrace for simple and exception log entry 2');
-INSERT INTO TEST2527_LOG VALUES (9, '2008-12-31 12:34:56', 'test4', 'level', 'simple and exception log entry 3');
+INSERT INTO test2527_log VALUES (7, '2008-12-31 12:34:56', 'test4', 'level', 'simple and exception log entry 1');
+INSERT INTO test2527_log VALUES (8, '2008-12-31 12:34:56', 'test4', 'level', 'simple and exception log entry 2');
+INSERT INTO test2527_log_exception VALUES (3, 8, 'stacktrace for simple and exception log entry 2');
+INSERT INTO test2527_log VALUES (9, '2008-12-31 12:34:56', 'test4', 'level', 'simple and exception log entry 3');
 
-INSERT INTO TEST2527_LOG VALUES (10, '2008-12-31 12:34:56', 'test5', 'level', 'simple and refering log entry 1');
-INSERT INTO TEST2527_LOG VALUES (11, '2008-12-31 12:34:56', 'test5', 'level', 'simple and refering log entry 2');
-INSERT INTO TEST2527_LOG_REFERENCE VALUES (11, 'type', 's+r 2');
-INSERT INTO TEST2527_LOG VALUES (12, '2008-12-31 12:34:56', 'test5', 'level', 'simple and refering log entry 3');
+INSERT INTO test2527_log VALUES (10, '2008-12-31 12:34:56', 'test5', 'level', 'simple and refering log entry 1');
+INSERT INTO test2527_log VALUES (11, '2008-12-31 12:34:56', 'test5', 'level', 'simple and refering log entry 2');
+INSERT INTO test2527_log_reference VALUES (11, 'type', 's+r 2');
+INSERT INTO test2527_log VALUES (12, '2008-12-31 12:34:56', 'test5', 'level', 'simple and refering log entry 3');
 
-INSERT INTO TEST2527_LOG VALUES (13, '2008-12-31 12:34:56', 'test6', 'level', 'exception, refering and simple log entry 1');
-INSERT INTO TEST2527_LOG_EXCEPTION VALUES (4, 13, 'stacktrace for exception, refering and simple log entry 1');
-INSERT INTO TEST2527_LOG VALUES (14, '2008-12-31 12:34:56', 'test6', 'level', 'exception, refering and simple log entry 2');
-INSERT INTO TEST2527_LOG_REFERENCE VALUES (14, 'type', 'e+r+s 2');
-INSERT INTO TEST2527_LOG VALUES (15, '2008-12-31 12:34:56', 'test6', 'level', 'exception, refering and simple log entry 3');
+INSERT INTO test2527_log VALUES (13, '2008-12-31 12:34:56', 'test6', 'level', 'exception, refering and simple log entry 1');
+INSERT INTO test2527_log_exception VALUES (4, 13, 'stacktrace for exception, refering and simple log entry 1');
+INSERT INTO test2527_log VALUES (14, '2008-12-31 12:34:56', 'test6', 'level', 'exception, refering and simple log entry 2');
+INSERT INTO test2527_log_reference VALUES (14, 'type', 'e+r+s 2');
+INSERT INTO test2527_log VALUES (15, '2008-12-31 12:34:56', 'test6', 'level', 'exception, refering and simple log entry 3');
