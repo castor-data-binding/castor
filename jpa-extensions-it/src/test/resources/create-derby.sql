b3489572197401cf3405cc4d870010e22a9e6dfd
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2009-06-13 20:22:24 +0000

    Finalized Derby SQL statements for table creation.

--- jpa-extensions-it/src/test/resources/create-derby.sql
+++ jpa-extensions-it/src/test/resources/create-derby.sql
@@ -1,44 +1,60 @@
 CREATE SCHEMA TEST;
 
-CREATE TABLE broadcast(
-    id INTEGER NOT NULL,
-    title VARCHAR(200) NOT NULL,
-    actor_id INTEGER NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE actor(
-    id INTEGER NOT NULL,
-    firstname VARCHAR(50) NOT NULL,
-    surname VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-ALTER TABLE broadcast ADD CONSTRAINT fk_actor_id
-	FOREIGN KEY (actor_id) REFERENCES actor (id);
-	
-
-
-//CREATE TABLE show(
-//   id INTEGER NOT NULL,
-//    name VARCHAR(200) NOT NULL,
-//    PRIMARY KEY (id),
-//    FOREIGN KEY (timeslot_id)
-//        REFERENCES timeslot(id)
-//);
-
-//CREATE TABLE timeslot(
-//    id INTEGER NOT NULL,
-//    name VARCHAR(200) NOT NULL,
-//    PRIMARY KEY(id),
-//    FOREIGN KEY (show_id)
-//    	REFERENCES show(id)
-//);
-
-CREATE TABLE staff(
+CREATE TABLE single_staff(
     id INTEGER NOT NULL,
     name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     pay NUMERIC(7,2) NOT NULL,
     PRIMARY KEY(id)
+);
+
+CREATE TABLE OneToOne_show(
+    id INTEGER NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    timeslot_id INTEGER NOT NULL,
+    rating_id INTEGER,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE OneToOne_timeslot(
+    id INTEGER NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE OneToOne_rating(
+    id INTEGER NOT NULL,
+    value INTEGER NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE OneToMany_actor(
+    svnr INTEGER NOT NULL,
+    firstname VARCHAR(200) NOT NULL,
+    surname VARCHAR(200) NOT NULL,
+    PRIMARY KEY (svnr)
+);
+
+CREATE TABLE OneToMany_role(
+    id INTEGER NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    actor_id INTEGER NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE ManyToMany_author(
+    id INTEGER NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE ManyToMany_book(
+    isbn INTEGER NOT NULL,
+    title VARCHAR(200) NOT NULL,
+    PRIMARY KEY (isbn)
+);
+
+CREATE TABLE ManyToMany_books_authors(
+    book_id INTEGER NOT NULL,
+    author_id INTEGER NOT NULL
 );
