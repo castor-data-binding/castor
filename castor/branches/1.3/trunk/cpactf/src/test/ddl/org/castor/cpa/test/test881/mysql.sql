9ce2ae1a65291541881e5d7391943c0d88968942
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2009-07-30 20:24:17 +0000

    Should never have been there ....

--- castor/branches/1.3/trunk/cpactf/src/test/ddl/org/castor/cpa/test/test881/mysql.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists test881_prod;
-
-create table test881_prod (
-  id        int           not null,
-  name      varchar(200)  not null
-);
-
-insert into test881_prod values (1, 'This is the test object.');
--- castor/branches/1.3/trunk/cpactf/src/test/ddl/org/castor/cpa/test/test954/oracle.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE test954_prod CASCADE CONSTRAINTS;
-
-CREATE TABLE test954_prod (
-  id        int           not null,
-  name      varchar(200)  not null
-);
-
-INSERT INTO test954_prod VALUES (1, 'This is the test object.');
