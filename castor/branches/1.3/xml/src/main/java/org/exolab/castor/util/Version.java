2356477189cfe03ae76b221a1ec41e35fc95c606
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2009-08-11 21:56:03 +0000

    Increased version to 1.3.0.1

--- castor/branches/1.3/xml/src/main/java/org/exolab/castor/util/Version.java
+++ castor/branches/1.3/xml/src/main/java/org/exolab/castor/util/Version.java
@@ -67,12 +67,12 @@ public final class Version {
     /**
      * The version number
      */
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.3.0.1";
         
     /**
      * The version date.
      */
-    public static final String VERSION_DATE  = "20091002";
+    public static final String VERSION_DATE  = "20090813";
     
     /**
      * The version number with build information
