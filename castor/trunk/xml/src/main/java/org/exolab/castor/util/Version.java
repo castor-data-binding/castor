b485cdd569b7fc04a0415c94c5d7e01187d9afe0
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2009-09-02 20:31:51 +0000

    Increased build number.

--- castor/trunk/xml/src/main/java/org/exolab/castor/util/Version.java
+++ castor/trunk/xml/src/main/java/org/exolab/castor/util/Version.java
@@ -67,7 +67,7 @@ public final class Version {
     /**
      * The version number
      */
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.3.1";
         
     /**
      * The version date.
