b115c92614f97f3f79d675107ef602150bbdc2c8
mc.schroeder <mc.schroeder@b24b0d9a-6811-0410-802a-946fa971d308>
2010-01-21 09:55:34 +0000

    cpactf test2868

---
+++ castor/branches/castor-ase-09/cpactf/src/test/java/org/castor/cpa/test/test2868/Book.java
@@ -0,0 +1,15 @@
+package org.castor.cpa.test.test2868;
+
+import org.junit.Ignore;
+
+@Ignore
+public class Book {
+    private int id;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+}
