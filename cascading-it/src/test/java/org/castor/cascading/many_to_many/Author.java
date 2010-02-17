ee9e22dba2b6cabd51ac5cfa088b8359c68a65bd
mc.schroeder <mc.schroeder@b24b0d9a-6811-0410-802a-946fa971d308>
2010-02-17 16:57:07 +0000

    added: many-to-many test structure

---
+++ cascading-it/src/test/java/org/castor/cascading/many_to_many/Author.java
@@ -0,0 +1,54 @@
+package org.castor.cascading.many_to_many;
+
+import java.util.Collection;
+
+import org.exolab.castor.jdo.TimeStampable;
+import org.junit.Ignore;
+
+@Ignore
+public class Author implements TimeStampable {
+	private int id;
+	private long timestamp;
+	private String name;
+	private Collection<Book> books;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public long jdoGetTimeStamp() {
+		return timestamp;
+	}
+
+	public void jdoSetTimeStamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setBooks(Collection<Book> books) {
+	    this.books = books;
+	}
+
+	public Collection<Book> getBooks() {
+	    return books;
+	}
+}
