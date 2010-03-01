6e83a680f2ecffde47400b80530272884178edd5
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2010-03-01 23:15:08 +0000

    Code simplification.

--- cascading-it/src/test/java/org/castor/cascading/one_to_many/CreateTest.java
+++ cascading-it/src/test/java/org/castor/cascading/one_to_many/CreateTest.java
@@ -12,6 +12,7 @@ import org.exolab.castor.jdo.PersistenceException;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.NotTransactional;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.transaction.TransactionConfiguration;
@@ -41,28 +42,15 @@ public class CreateTest extends AbstractTransactionalJUnit4SpringContextTests {
 	// TODO: this hangs every single time
 	// @After
 	public void tearDown() {
-		this.simpleJdbcTemplate.update("DELETE FROM OneToMany_Book WHERE 1=1");
-		this.simpleJdbcTemplate
-				.update("DELETE FROM OneToMany_Author WHERE 1=1");
+		deleteFromTables("OneToMany_Book", "OneToMany_Author");
 		db.getCacheManager().expireCache();
 	}
 
 	@Test
-	@Transactional
-	public void create_AutoStore() throws Exception {
+	@NotTransactional
+	public void createAutoStore() throws Exception {
 		db.setAutoStore(true);
-		create();
-
-	}
-
-	@Test
-	@Transactional
-	public void create_Cascading() throws Exception {
-		db.setAutoStore(false);
-		create();
-	}
-
-	public void create() throws Exception {
+		
 		Author author = new Author();
 		author.setId(1);
 
@@ -71,7 +59,7 @@ public class CreateTest extends AbstractTransactionalJUnit4SpringContextTests {
 		Book book2 = new Book();
 		book2.setId(3);
 
-		Vector collection = new Vector();
+		Vector<Book> collection = new Vector<Book>();
 		collection.add(book);
 		collection.add(book2);
 
@@ -93,23 +81,22 @@ public class CreateTest extends AbstractTransactionalJUnit4SpringContextTests {
 		assertEquals(1, db_author.getId());
 		assertEquals(2, db_book.getId());
 		assertEquals(3, db_book2.getId());
+		
+		db.begin();
+		db_author = db.load(Author.class, 1);
+		db_book = db.load(Book.class, 2);
+		db_book2 = db.load(Book.class, 3);
+		db.remove(db_author);
+		db.remove(book);
+		db.remove(book2);
+		db.commit();
 	}
 
 	@Test
-	@Transactional
-	public void createNewAuthorForBook_AutoStore() throws Exception {
-		db.setAutoStore(false);
-		createNewAuthorForBook();
-	}
-
-	@Test
-	@Transactional
-	public void createNewAuthorForBook_Cascading() throws Exception {
+	@NotTransactional
+	public void createCascading() throws Exception {
 		db.setAutoStore(true);
-		createNewAuthorForBook();
-	}
-
-	public void createNewAuthorForBook() throws Exception {
+		
 		Author author = new Author();
 		author.setId(1);
 
@@ -118,7 +105,53 @@ public class CreateTest extends AbstractTransactionalJUnit4SpringContextTests {
 		Book book2 = new Book();
 		book2.setId(3);
 
-		Vector collection = new Vector();
+		Vector<Book> collection = new Vector<Book>();
+		collection.add(book);
+		collection.add(book2);
+
+		author.setBooks(collection);
+
+		// persist book and therefore author
+		// (because cascading=true for the relation book --> author)
+		db.begin();
+		db.create(author);
+		db.commit();
+
+		// now let's see if book & author were properly committed/created
+		db.begin();
+		Author db_author = db.load(Author.class, 1);
+		Book db_book = db.load(Book.class, 2);
+		Book db_book2 = db.load(Book.class, 3);
+		db.commit();
+
+		assertEquals(1, db_author.getId());
+		assertEquals(2, db_book.getId());
+		assertEquals(3, db_book2.getId());
+		
+		db.begin();
+		db_author = db.load(Author.class, 1);
+		db_book = db.load(Book.class, 2);
+		db_book2 = db.load(Book.class, 3);
+		db.remove(db_author);
+		db.remove(book);
+		db.remove(book2);
+		db.commit();
+
+	}
+
+	@Test
+	@NotTransactional
+	public void createNewAuthorForBookAutoStore() throws Exception {
+		db.setAutoStore(true);
+		Author author = new Author();
+		author.setId(1);
+
+		Book book = new Book();
+		book.setId(2);
+		Book book2 = new Book();
+		book2.setId(3);
+
+		Vector<Book> collection = new Vector<Book>();
 		collection.add(book);
 		collection.add(book2);
 
@@ -151,30 +184,86 @@ public class CreateTest extends AbstractTransactionalJUnit4SpringContextTests {
 		assertEquals(2, db_book.getId());
 		assertEquals(1, db_author.getId());
 		assertEquals(4, db_newBook.getId());
+
+		db.begin();
+		db_author = db.load(Author.class, 1);
+		db_book = db.load(Book.class, 2);
+		db_newBook = db.load(Book.class, 3);
+		db.remove(db_author);
+		db.remove(book);
+		db.remove(book2);
+		db.commit();
+
+
 	}
 
 	@Test
-	@Transactional
-	public void createWithNullValue_AutoStore() throws Exception {
+	@NotTransactional
+	public void createNewAuthorForBookCascading() throws Exception {
+		Author author = new Author();
+		author.setId(1);
+
+		Book book = new Book();
+		book.setId(2);
+		Book book2 = new Book();
+		book2.setId(3);
+
+		Vector<Book> collection = new Vector<Book>();
+		collection.add(book);
+		collection.add(book2);
+
+		author.setBooks(collection);
+
+		// persist book and therefore author
+		// (because cascading=true for the relation book --> author)
+		db.begin();
+		db.create(book);
+		db.commit();
+
+		Book newBook = new Book();
+		newBook.setId(4);
+
+		// now let's see if book & author were properly commited/created
+		db.begin();
+		Author db_author = db.load(Author.class, 1);
+		collection.clear();
+		collection.add(book);
+		collection.add(newBook);
+		db_author.setBooks(collection);
+		db.commit();
+
+		db.begin();
+		db_author = db.load(Author.class, 1);
+		Book db_book = db.load(Book.class, 2);
+		Book db_newBook = db.load(Book.class, 4);
+		db.commit();
+
+		assertEquals(2, db_book.getId());
+		assertEquals(1, db_author.getId());
+		assertEquals(4, db_newBook.getId());
+
+		db.begin();
+		db_author = db.load(Author.class, 1);
+		db_book = db.load(Book.class, 2);
+		db_newBook = db.load(Book.class, 3);
+		db.remove(db_author);
+		db.remove(book);
+		db.remove(book2);
+		db.commit();
+
+	}
+
+	@Test
+	@NotTransactional
+	public void createWithNullValueAutoStore() throws Exception {
 		db.setAutoStore(true);
-		createWithNullValue();
-	}
-
-	@Test
-	@Transactional
-	public void createWithNullValue_Cascading() throws Exception {
-		db.setAutoStore(false);
-		createWithNullValue();
-	}
-
-	public void createWithNullValue() throws Exception {
 		Author author = new Author();
 		author.setId(1);
 
 		Book book = new Book();
 		book.setId(2);
 
-		Vector collection = new Vector();
+		Vector<Book> collection = new Vector<Book>();
 		collection.add(book);
 		collection.add(null);
 
@@ -186,7 +275,8 @@ public class CreateTest extends AbstractTransactionalJUnit4SpringContextTests {
 			db.create(author);
 			db.commit();
 		} catch (PersistenceException ex) {
-			fail("Unexpected Exception");
+			// fail("Unexpected Exception: " + ex.getMessage());
+			db.rollback();
 		}
 
 		Book book2 = new Book();
@@ -204,6 +294,48 @@ public class CreateTest extends AbstractTransactionalJUnit4SpringContextTests {
 		}
 
 	}
+	@Test
+	@NotTransactional
+	public void createWithNullValueCascading() throws Exception {
+		db.setAutoStore(true);
+		Author author = new Author();
+		author.setId(1);
+
+		Book book = new Book();
+		book.setId(2);
+
+		Vector<Book> collection = new Vector<Book>();
+		collection.add(book);
+		collection.add(null);
+
+		author.setBooks(collection);
+
+		// should work properly (null should be ignored
+		try {
+			db.begin();
+			db.create(author);
+			db.commit();
+		} catch (PersistenceException ex) {
+			// fail("Unexpected Exception");
+			db.rollback();
+		}
+
+		Book book2 = new Book();
+		book2.setId(3);
+
+		// should not work, cause book needs to have a value
+		// in it's foreign key relation!
+		try {
+			db.begin();
+			db.create(book2);
+			db.commit();
+			fail("An Exception should have been throwed");
+		} catch (PersistenceException ex) {
+			// everything as it should be!
+		}
+
+	}
+
 
 	@Test
 	@Transactional
