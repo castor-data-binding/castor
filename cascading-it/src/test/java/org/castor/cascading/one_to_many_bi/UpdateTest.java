ee8c0c12732ca70d7e4ece2b574df73220192380
mc.schroeder <mc.schroeder@b24b0d9a-6811-0410-802a-946fa971d308>
2010-03-09 21:48:21 +0000

    fixed otm_bi update test

--- cascading-it/src/test/java/org/castor/cascading/one_to_many_bi/UpdateTest.java
+++ cascading-it/src/test/java/org/castor/cascading/one_to_many_bi/UpdateTest.java
@@ -17,8 +17,6 @@ import org.springframework.test.context.junit4.AbstractTransactionalJUnit4Spring
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
-//TODO: The OneToMany mapping seems very corrupt. tests should work after fixing this!
-
 /**
  * Verifies the correct behaviour of cascading set to "create".
  * 
@@ -36,44 +34,89 @@ public class UpdateTest extends AbstractTransactionalJUnit4SpringContextTests {
 	@Before
 	public void setUp() throws Exception {
 		db = jdoManager.getDatabase();
-	}
-
-	// TODO: this hangs every single time
-	// @After
-	public void tearDown() {
-		this.simpleJdbcTemplate
-				.update("DELETE FROM OneToMany_Bi_Book WHERE 1=1");
-		this.simpleJdbcTemplate
-				.update("DELETE FROM OneToMany_Bi_Author WHERE 1=1");
-		db.getCacheManager().expireCache();
+		deleteFromTables("OneToMany_Bi_Book", "OneToMany_Bi_Author");
 	}
 
 	@Test
 	@Transactional
 	public void changeBook_AutoStore() throws Exception {
 		db.setAutoStore(true);
-		changeBook();
+		
+		Author author = new Author();
+		author.setId(11);
+		author.setName("Jack");
+
+		Book book1 = new Book();
+		book1.setId(12);
+		book1.setName("book1");
+		book1.setAuthor(author);
+
+		Book book2 = new Book();
+		book2.setId(13);
+		book2.setName("book2");
+		book2.setAuthor(author);
+
+		Collection<Book> books = Arrays.asList(book1, book2);
+		author.setBooks(books);
+
+		// create objects
+		db.begin();
+		db.create(book1);
+		db.create(book2);
+		db.create(author);
+		db.commit();
+
+		// load objects
+		db.begin();
+		Author db_author = db.load(Author.class, 11);
+		db.commit();
+
+		// change author
+		Iterator<Book> iter = db_author.getBooks().iterator();
+		while (iter.hasNext()) {
+			Book b = iter.next();
+			if (b.getId() == 12) {
+				b.setName("new name for book1");
+			}
+		}
+
+		// update objects
+		db.begin();
+		db.update(db_author);
+		db.commit();
+
+		// load objects again and check
+		db.begin();
+		Author db_author2 = db.load(Author.class, 11);
+		db.commit();
+
+		iter = db_author2.getBooks().iterator();
+		while (iter.hasNext()) {
+			Book b = iter.next();
+			if (b.getId() == 12) {
+				assertEquals(b.getName(), "new name for book1");
+			}
+		}
 	}
 
 	@Test
 	@Transactional
 	public void changeBook_Cascading() throws Exception {
 		db.setAutoStore(false);
-		changeBook();
-	}
+		
+		Author author = new Author();
+		author.setId(11);
+		author.setName("Jack");
 
-	public void changeBook() throws Exception {
 		Book book1 = new Book();
 		book1.setId(12);
 		book1.setName("book1");
+		book1.setAuthor(author);
 
 		Book book2 = new Book();
 		book2.setId(13);
 		book2.setName("book2");
-
-		Author author = new Author();
-		author.setId(11);
-		author.setName("Jack");
+		book2.setAuthor(author);
 
 		Collection<Book> books = Arrays.asList(book1, book2);
 		author.setBooks(books);
@@ -122,28 +165,70 @@ public class UpdateTest extends AbstractTransactionalJUnit4SpringContextTests {
 	@Transactional
 	public void removeBook_AutoStore() throws Exception {
 		db.setAutoStore(true);
-		removeBook();
+		
+		Author author = new Author();
+		author.setId(11);
+		author.setName("Jack");
+
+		Book book1 = new Book();
+		book1.setId(12);
+		book1.setName("book1");
+		book1.setAuthor(author);
+
+		Book book2 = new Book();
+		book2.setId(13);
+		book2.setName("book2");
+		book2.setAuthor(author);
+
+		author.setBooks(Arrays.asList(book1, book2));
+
+		// create objects
+		db.begin();
+		db.create(book1);
+		db.create(book2);
+		db.create(author);
+		db.commit();
+
+		// load objects
+		db.begin();
+		Author db_author = db.load(Author.class, 11);
+		db.commit();
+
+		// remove book
+		db_author.setBooks(Arrays.asList(book2));
+
+		// update objects
+		db.begin();
+		db.update(db_author);
+		db.commit();
+
+		// load objects again and check
+		db.begin();
+		Author db_author2 = db.load(Author.class, 11);
+		db.commit();
+
+		assertEquals(db_author2.getBooks().size(), 1);
+		assertEquals(((Book) db_author2.getBooks().toArray()[0]).getId(), 13);
 	}
 
 	@Test
 	@Transactional
 	public void removeBook_Cascading() throws Exception {
 		db.setAutoStore(false);
-		removeBook();
-	}
+		
+		Author author = new Author();
+		author.setId(11);
+		author.setName("Jack");
 
-	public void removeBook() throws Exception {
 		Book book1 = new Book();
 		book1.setId(12);
 		book1.setName("book1");
+		book1.setAuthor(author);
 
 		Book book2 = new Book();
 		book2.setId(13);
 		book2.setName("book2");
-
-		Author author = new Author();
-		author.setId(11);
-		author.setName("Jack");
+		book2.setAuthor(author);
 
 		author.setBooks(Arrays.asList(book1, book2));
 
@@ -180,28 +265,72 @@ public class UpdateTest extends AbstractTransactionalJUnit4SpringContextTests {
 	@Transactional
 	public void addBook_AutoStore() throws Exception {
 		db.setAutoStore(true);
-		addBook();
+		
+		Author author = new Author();
+		author.setId(11);
+		author.setName("Jack");
+		
+		Book book1 = new Book();
+		book1.setId(12);
+		book1.setName("book1");
+		book1.setAuthor(author);
+
+		Book book2 = new Book();
+		book2.setId(13);
+		book2.setName("book2");
+		book2.setAuthor(author);
+
+		author.setBooks(Arrays.asList(book1, book2));
+
+		// create objects
+		db.begin();
+		db.create(book1);
+		db.create(book2);
+		db.create(author);
+		db.commit();
+
+		// load objects
+		db.begin();
+		Author db_author = db.load(Author.class, 11);
+		db.commit();
+
+		// add book
+		Book book3 = new Book();
+		book3.setId(14);
+		book3.setName("book3");
+		db_author.setBooks(Arrays.asList(book1, book2, book3));
+
+		// update objects
+		db.begin();
+		db.update(db_author);
+		db.commit();
+
+		// load objects again and check
+		db.begin();
+		Author db_author2 = db.load(Author.class, 11);
+		db.commit();
+
+		assertEquals(db_author2.getBooks().size(), 3);
 	}
 
 	@Test
 	@Transactional
 	public void addBookCascading() throws Exception {
 		db.setAutoStore(false);
-		addBook();
-	}
-
-	public void addBook() throws Exception {
+		
+		Author author = new Author();
+		author.setId(11);
+		author.setName("Jack");
+		
 		Book book1 = new Book();
 		book1.setId(12);
 		book1.setName("book1");
+		book1.setAuthor(author);
 
 		Book book2 = new Book();
 		book2.setId(13);
 		book2.setName("book2");
-
-		Author author = new Author();
-		author.setId(11);
-		author.setName("Jack");
+		book2.setAuthor(author);
 
 		author.setBooks(Arrays.asList(book1, book2));
 
@@ -240,28 +369,20 @@ public class UpdateTest extends AbstractTransactionalJUnit4SpringContextTests {
 	@Transactional
 	public void withNullValue_AutoStore() throws Exception {
 		db.setAutoStore(true);
-		withNullValue();
-	}
-
-	@Test
-	@Transactional
-	public void withNullValue_Cascading() throws Exception {
-		db.setAutoStore(false);
-		withNullValue();
-	}
-
-	public void withNullValue() throws Exception {
+		
+		Author author = new Author();
+		author.setId(11);
+		author.setName("Jack");
+		
 		Book book1 = new Book();
 		book1.setId(12);
 		book1.setName("book1");
+		book1.setAuthor(author);
 
 		Book book2 = new Book();
 		book2.setId(13);
 		book2.setName("book2");
-
-		Author author = new Author();
-		author.setId(11);
-		author.setName("Jack");
+		book2.setAuthor(author);
 
 		author.setBooks(Arrays.asList(book1, book2));
 
@@ -291,4 +412,50 @@ public class UpdateTest extends AbstractTransactionalJUnit4SpringContextTests {
 		}
 	}
 
+	@Test
+	@Transactional
+	public void withNullValue_Cascading() throws Exception {
+		db.setAutoStore(false);
+		
+		Author author = new Author();
+		author.setId(11);
+		author.setName("Jack");
+		
+		Book book1 = new Book();
+		book1.setId(12);
+		book1.setName("book1");
+		book1.setAuthor(author);
+
+		Book book2 = new Book();
+		book2.setId(13);
+		book2.setName("book2");
+		book2.setAuthor(author);
+
+		author.setBooks(Arrays.asList(book1, book2));
+
+		// create objects
+		db.begin();
+		db.create(book1);
+		db.create(book2);
+		db.create(author);
+		db.commit();
+
+		// load objects
+		db.begin();
+		Author db_author = db.load(Author.class, 11);
+		db.commit();
+
+		// set null
+		db_author.setBooks(null);
+
+		// update objects
+		try {
+			db.begin();
+			db.update(db_author);
+			db.commit();
+			fail("Exception expected");
+		} catch (Exception e) {
+			// ok
+		}
+	}
 }
