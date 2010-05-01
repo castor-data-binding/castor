2733c66ef7b588aae30b4b41775bc4d01cda4161
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2010-05-01 15:16:10 +0000

    Added SQL file top (easily) drop tables in Derby.

---
+++ jpa-extensions-it/src/test/resources/drop-derby.sql
@@ -0,0 +1,6 @@
+DROP TABLE test.single_staff;
+DROP TABLE test.OneToOne_show;
+DROP TABLE test.OneToOne_timeslot;
+DROP TABLE test.OneToOne_Rating;
+DROP TABLE test.callbacks_person;
+DROP TABLE test.person;
