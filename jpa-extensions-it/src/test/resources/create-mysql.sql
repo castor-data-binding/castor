23d7413b3e55de435bc4e908691580fe1b209494
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2010-05-01 15:15:48 +0000

    Added missing DROP statement.

--- jpa-extensions-it/src/test/resources/create-mysql.sql
+++ jpa-extensions-it/src/test/resources/create-mysql.sql
@@ -1,3 +1,10 @@
+DROP TABLE IF EXISTS callbacks_person;
+CREATE TABLE callbacks_person(
+    id INTEGER NOT NULL,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id)
+);
+
 DROP TABLE IF EXISTS single_staff;
 CREATE TABLE single_staff(
     id INTEGER NOT NULL,
@@ -25,43 +32,43 @@ CREATE TABLE OneToOne_timeslot(
 
 DROP TABLE IF EXISTS OneToOne_rating;
 CREATE TABLE OneToOne_rating(
-	id INTEGER NOT NULL,
-	value INTEGER NOT NULL,
-	PRIMARY KEY (id)
+    id INTEGER NOT NULL,
+    value INTEGER NOT NULL,
+    PRIMARY KEY (id)
 )ENGINE=INNODB;
 
 DROP TABLE IF EXISTS OneToMany_actor;
 CREATE TABLE OneToMany_actor(
-	svnr INTEGER NOT NULL,
-	firstname VARCHAR(200) NOT NULL,
-	surname VARCHAR(200) NOT NULL,
-	PRIMARY KEY (svnr)
+    svnr INTEGER NOT NULL,
+    firstname VARCHAR(200) NOT NULL,
+    surname VARCHAR(200) NOT NULL,
+    PRIMARY KEY (svnr)
 )ENGINE=INNODB;
 
 DROP TABLE IF EXISTS OneToMany_role;
 CREATE TABLE OneToMany_role(
-	id INTEGER NOT NULL,
-	name VARCHAR(200) NOT NULL,
-	actor_id INTEGER NOT NULL,
-	PRIMARY KEY (id)
+    id INTEGER NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    actor_id INTEGER NOT NULL,
+    PRIMARY KEY (id)
 )ENGINE=INNODB;
 
 DROP TABLE IF EXISTS ManyToMany_author;
 CREATE TABLE ManyToMany_author(
-	id INTEGER NOT NULL,
-	name VARCHAR(200) NOT NULL,
-	PRIMARY KEY (id)
+    id INTEGER NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    PRIMARY KEY (id)
 )ENGINE=INNODB;
 
 DROP TABLE IF EXISTS ManyToMany_book;
 CREATE TABLE ManyToMany_book(
-	isbn INTEGER NOT NULL,
-	title VARCHAR(200) NOT NULL,
-	PRIMARY KEY (isbn)
+    isbn INTEGER NOT NULL,
+    title VARCHAR(200) NOT NULL,
+    PRIMARY KEY (isbn)
 )ENGINE=INNODB;
 
 DROP TABLE IF EXISTS ManyToMany_books_authors;
 CREATE TABLE ManyToMany_books_authors(
-	book_id INTEGER NOT NULL,
-	author_id INTEGER NOT NULL
+    book_id INTEGER NOT NULL,
+    author_id INTEGER NOT NULL
 )ENGINE=INNODB;
