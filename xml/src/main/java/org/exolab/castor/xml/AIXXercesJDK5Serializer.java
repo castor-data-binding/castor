f6a9de92f1d768beb0b7f24175a4b38a222e2123
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2010-05-06 19:39:47 +0000

    Added support for Xerces as part of the JDK as serializer on AIX (CASTOR-2905).

--- xml/src/main/java/org/exolab/castor/xml/AIXXercesJDK5Serializer.java
+++ xml/src/main/java/org/exolab/castor/xml/AIXXercesJDK5Serializer.java
@@ -17,23 +17,24 @@ package org.exolab.castor.xml;
 
 
 /**
- * Xerces-specific implementation of the Serializer interface, used for
- * JDK 5 only where Xerces has been integrated with the core code base.
+ * Xerces-specific implementation of the Serializer interface for AIX, used for
+ * JDK 5 on AIX only where Xerces has been integrated with the core code base.
  * 
  * @author <a href="mailto:werner DOT guttmann AT gmx DOT net">Werner Guttmann</a>
  * @version $Revision: 6216 $ $Date: 2006-04-25 16:09:10 -0600 (Tue, 25 Apr 2006) $
- * @since 1.1
+ * @since 1.3.2
  * 
  * @see com.sun.org.apache.xml.internal.serialize.Serializer
  */
-public class XercesJDK5Serializer extends BaseXercesJDK5Serializer {
+public class AIXXercesJDK5Serializer extends BaseXercesJDK5Serializer {
     
-    private static final String PACKAGE_NAME = "com.sun.org.apache.xml.internal.serialize";
+    private static final String PACKAGE_NAME = "com.apache.xml.serialize";
 
     @Override
     protected String getPackageName() {
         return PACKAGE_NAME;
-    } 
-
+    }
+    
+    
     
 }
