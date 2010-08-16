75ba5f56cfe3e26b1b59bb53d1f8f508c3852bb8
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2010-08-16 19:45:05 +0000

    Improved Javadocs.

--- castor/trunk/xml/src/main/java/org/exolab/castor/xml/XMLSerializerFactory.java
+++ castor/trunk/xml/src/main/java/org/exolab/castor/xml/XMLSerializerFactory.java
@@ -18,6 +18,22 @@ package org.exolab.castor.xml;
 /**
  * Configurable factory object for XML serialization.
  * 
+ * The XML serializer factory is set through the 
+ * <tt>org.exolab.castor.xml.serializer.factory</tt> in the
+ * <tt>castor.properties</tt> file shipped with the product JAR.
+ * By default, this is set to a value of 
+ * <tt>org.exolab.castor.xml.XercesJDK5XMLSerializerFactory</tt>. This
+ * {@link XMLSerializerFactory} will use the Xerces instance as shipped with 
+ * the JRE.
+ * 
+ * To use another {@link XMLSerializerFactory}, please override this 
+ * property in a custom <tt>castor.properties</tt>.
+ * 
+ * When using Castor XML with JDK 5.0 and above, you may switch to 
+ * the {@link XercesXMLSerializerFactory}, which will use a separately 
+ * downloaded Xerces instance, and not the one shipped with the JRE
+ * itself. 
+ * 
  * @author <a href="mailto:werner DOT guttmann AT gmx DOT net">Werner Guttmann</a>
  * @version $Revision$ $Date: 2006-04-25 16:09:10 -0600 (Tue, 25 Apr
  *          2006) $
