891c1d19b060fdf06e4aa063c224fa86788adf06
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2011-03-29 20:19:46 +0000

    New version numbers for non-Maven binaries

--- xml/src/main/java/org/exolab/castor/util/Version.java
+++ xml/src/main/java/org/exolab/castor/util/Version.java
@@ -67,12 +67,12 @@ public final class Version {
     /**
      * The version number
      */
-    public static final String VERSION = "1.3.1";
+    public static final String VERSION = "1.3.2";
         
     /**
      * The version date.
      */
-    public static final String VERSION_DATE  = "20100103";
+    public static final String VERSION_DATE  = "20110329";
     
     /**
      * The version number with build information
