0b7754d7c8110be607b5eb39ddd8abed08b49752
rjoachim <rjoachim@b24b0d9a-6811-0410-802a-946fa971d308>
2011-08-08 22:23:03 +0000

    Property changes don't get persisted in extends hierarchy (CASTOR-3183)

---
+++ cpactf/src/test/ddl/org/castor/cpa/test/test3183/derby-create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE test3183_poly_doc (
+  id    int           NOT NULL, 
+  name  varchar(20)   NOT NULL
+);
+
+CREATE TABLE test3183_poly_order_doc (
+  id    int           NOT NULL, 
+  price double        NOT NULL
+);
+
+
+CREATE TABLE test3183_single_order_doc (
+  id    int           NOT NULL, 
+  name  varchar(20)   NOT NULL,
+  price double        NOT NULL
+);
