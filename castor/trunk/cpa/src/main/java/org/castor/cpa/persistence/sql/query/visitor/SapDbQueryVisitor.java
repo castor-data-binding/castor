b956b29d944e323eea209ca63de332334f58abec
rjoachim <rjoachim@b24b0d9a-6811-0410-802a-946fa971d308>
2011-08-23 21:25:04 +0000

    Join SQL Generation for SAPDB/MaxDB are wrong (CASTOR-3189)

--- castor/trunk/cpa/src/main/java/org/castor/cpa/persistence/sql/query/visitor/SapDbQueryVisitor.java
+++ castor/trunk/cpa/src/main/java/org/castor/cpa/persistence/sql/query/visitor/SapDbQueryVisitor.java
@@ -17,10 +17,16 @@
  */
 package org.castor.cpa.persistence.sql.query.visitor;
 
+import java.util.Iterator;
+import java.util.List;
 import java.util.StringTokenizer;
 
+import org.castor.cpa.persistence.sql.query.Join;
+import org.castor.cpa.persistence.sql.query.Qualifier;
 import org.castor.cpa.persistence.sql.query.QueryConstants;
 import org.castor.cpa.persistence.sql.query.Select;
+import org.castor.cpa.persistence.sql.query.condition.Condition;
+import org.castor.cpa.persistence.sql.query.expression.Expression;
 
 /**
  * Visitor defining special behavior of query building for SapDb database.
@@ -31,6 +37,98 @@ import org.castor.cpa.persistence.sql.query.Select;
  */
 public final class SapDbQueryVisitor extends DefaultQueryVisitor {
     //-----------------------------------------------------------------------------------
+
+    /**
+     * {@inheritDoc}
+     */
+    public void visit(final Join join) {
+        _queryString.append(join.getOperator().toString());
+        _queryString.append(QueryConstants.SPACE);
+        _queryString.append(QueryConstants.JOIN);
+        _queryString.append(QueryConstants.SPACE);
+
+        
+        addTableNames(join.getJoin());
+
+        Condition condition = join.getCondition();
+        if (condition != null) {
+            _queryString.append(QueryConstants.SPACE);
+            _queryString.append(QueryConstants.ON);
+            _queryString.append(QueryConstants.SPACE);
+            condition.accept(this);
+        }
+
+        handleJoinConstruction(join.getJoin());
+
+        if (LOG.isTraceEnabled()) { 
+            LOG.trace("Visit join: " + toString());
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void visit(final Select select) {
+        List<Expression> expressions = select.getSelect();
+        Condition condition = select.getCondition();
+
+        _queryString.append(QueryConstants.SELECT);
+        _queryString.append(QueryConstants.SPACE);
+
+        if (expressions.isEmpty()) {
+            _queryString.append(QueryConstants.STAR);
+        } else {
+            for (Iterator<Expression> iter = expressions.iterator(); iter.hasNext(); ) {
+                iter.next().accept(this);
+                if (iter.hasNext()) {
+                    _queryString.append(QueryConstants.SEPERATOR);
+                    _queryString.append(QueryConstants.SPACE);
+                }
+            }
+        }
+
+        _queryString.append(QueryConstants.SPACE);
+        _queryString.append(QueryConstants.FROM);
+        _queryString.append(QueryConstants.SPACE);
+
+        for (Iterator<Qualifier> iter = select.getFrom().iterator(); iter.hasNext(); ) {
+            Qualifier qualifier = iter.next();
+
+            addTableNames(qualifier);
+            
+            handleJoinConstruction(qualifier);
+
+            if (iter.hasNext()) {
+                _queryString.append(QueryConstants.SEPERATOR);
+                _queryString.append(QueryConstants.SPACE);
+            }
+        }
+
+        if (condition != null) {
+            _queryString.append(QueryConstants.SPACE);
+            _queryString.append(QueryConstants.WHERE);
+            _queryString.append(QueryConstants.SPACE);
+            condition.accept(this);
+        }
+
+        handleLock(select);
+
+        if (LOG.isTraceEnabled()) { 
+            LOG.trace("Visit select: " + toString());
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected void handleJoinConstruction(final Qualifier qualifier) {
+        if (qualifier.hasJoin()) {
+            for (Join join : qualifier.getJoins()) {
+                _queryString.append(QueryConstants.SPACE);
+                join.accept(this);
+            }
+        }
+    }
 
     /**
      * {@inheritDoc}
