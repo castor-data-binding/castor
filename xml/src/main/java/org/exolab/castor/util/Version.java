05e20d06827bc87a30cc3d2eea997b1ffe844af5
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2011-09-27 19:32:48 +0000

    Updated version information to next stable release

--- xml/src/main/java/org/exolab/castor/util/Version.java
+++ xml/src/main/java/org/exolab/castor/util/Version.java
@@ -67,7 +67,7 @@ public final class Version {
     /**
      * The version number
      */
-    public static final String VERSION = "1.3.2";
+    public static final String VERSION = "1.3.3";
         
     /**
      * The version date.
