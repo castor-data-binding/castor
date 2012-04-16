e9c97d9e3aebd8230c03a251bdd0d8ed0eeee0a4
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2012-04-16 19:35:24 +0000

    Added integration test case for regression fix for issue 3101 (CASTOR-3101)

---
+++ xmlctf/tests/MasterTestSuite/mapping/location/soleAttributeNested/ClassA.java
@@ -0,0 +1,22 @@
+public class ClassA {
+
+	private ClassB b;
+	private ClassC c;
+
+	public ClassB getB() {
+		return b;
+	}
+
+	public void setB(ClassB b) {
+		this.b = b;
+	}
+
+	public ClassC getC() {
+		return c;
+	}
+
+	public void setC(ClassC c) {
+		this.c = c;
+	}
+
+}
