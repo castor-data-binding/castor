73932d572df8a81249d4daf2cc6a113b3f1de172
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2012-05-05 20:11:43 +0000

    Added JUnit tests for ClonableFieldHandler and ClonableFieldHandlerMarker interfaces

---
+++ castor/trunk/xmlctf/src/test/java/org/castor/xmlctf/fieldhandler/Root.java
@@ -0,0 +1,26 @@
+package org.castor.xmlctf.fieldhandler;
+
+import java.util.Date;
+
+public class Root {
+
+   private Date date;
+   private Date date2;
+
+   public Date getDate() {
+      return this.date;
+   }
+
+   public void setDate(final Date date) {
+      this.date = date;
+   }
+
+   public Date getDate2() {
+      return this.date2;
+   }
+
+   public void setDate2(final Date date2) {
+      this.date2 = date2;
+   }
+
+}
