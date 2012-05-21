6fb062bc86ac1d62f77c987ed77ac47afea8062f
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2012-05-21 21:40:56 +0000

    Moved template for XML test case to xmlctf

---
+++ xmlctf/src/test/java/org/castor/xml/template/bug/Entity.java
@@ -0,0 +1,22 @@
+package org.castor.xml.template.bug;
+
+public final class Entity {
+   private Integer id;
+   private String name;
+
+   public Integer getId() {
+      return id;
+   }
+
+   public void setId(final Integer id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(final String name) {
+      this.name = name;
+   }
+}
