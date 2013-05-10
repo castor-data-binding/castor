6be8f067580bcc1cb347de8aa688921c777eb6e8
wguttmn <wguttmn@b24b0d9a-6811-0410-802a-946fa971d308>
2013-05-10 18:31:39 +0000

    Added additional support for mysql, postgres and derby

---
+++ castor/branches/1.3.1/cpactf/src/test/ddl/org/castor/cpa/test/test882/postgresql-create.sql
@@ -0,0 +1,16 @@
+CREATE TABLE test882_product ( 
+  id INT NOT NULL, 
+  name VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE test882_productdetail ( 
+  id INT NOT NULL, 
+  name varchar(200) NOT NULL, 
+  prod_id INT NOT NULL
+);
+
+CREATE TABLE test882_productdetaillazy ( 
+  id INT NOT NULL, 
+  name varchar(200) NOT NULL, 
+  prod_id INT NOT NULL
+);
