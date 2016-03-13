ca1a5514155d8a1bbf54b60cabf243c21a8ec84d
Anthony Whitford <anthony@whitford.com>
2016-03-13 12:15:58 -0700

    Added @Deprecated for JakartaOroEvaluator since Oro is a retired project.

--- xml/src/main/java/org/exolab/castor/util/JakartaOroEvaluator.java
+++ xml/src/main/java/org/exolab/castor/util/JakartaOroEvaluator.java
@@ -58,6 +58,7 @@
   * @author <a href="mailto:kvisco@intalio.com">Keith Visco</a>
   * @version $Revision$ $Date: 2003-03-03 00:05:44 -0700 (Mon, 03 Mar 2003) $
  **/
+ @Deprecated
  public class JakartaOroEvaluator 
     implements RegExpEvaluator
 {
